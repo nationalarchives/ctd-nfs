029d4390670e8c96b497a377aa6f9732b1c4c603
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-19 16:49:42 +0000

    replace Addressee dataclass with Details

--- src/containers.py
+++ src/containers.py
@@ -36,14 +36,6 @@ def make_warnings_mapping() -> dict:
 Occupier, Owner and Farmer will be initialised with lists of the relevant components from each row for that farm.
 """
 @dataclass
-class Addressee:
-    addressee_title: list[str] | str
-    addressee_individual_name: list[str] | str
-    addressee_group_names: list[str] | str
-    address: list[str] | str
-
-
-@dataclass
 class Details:
     title: list[str] | str
     individual_name: list[str] | str
@@ -64,7 +56,7 @@ class Farm:
     primary_farm_number: str
     additional_farms: list[str]
     farm_name: list[str]
-    addressee: Addressee
+    addressee: Details
     owner: Details
     farmer: Details
     acreage: list[str] | str
@@ -89,10 +81,10 @@ class Farm:
         self.primary_farm_number = stripped_data['primary_farm_number'],
         self.additional_farms = self.clean_value(stripped_data['additional_farms']),
         self.farm_name = self.clean_value(stripped_data['farm_name']),
-        self.addressee = Addressee(
-            addressee_title=stripped_data['addressee_title'],
-            addressee_individual_name=stripped_data['addressee_individual_name'],
-            addressee_group_names=self.clean_value(stripped_data['addressee_group_names']),
+        self.addressee = Details(
+            title=stripped_data['addressee_title'],
+            individual_name=stripped_data['addressee_individual_name'],
+            group_names=self.clean_value(stripped_data['addressee_group_names']),
             address=stripped_data['address']
         ),
         self.owner = Details(
