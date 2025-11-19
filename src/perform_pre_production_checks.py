11b300e14e519a04d8ad9680c049747418357496
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-19 10:02:14 +0000

    fix regular expression
    "0*" is covered by "\d+"

--- src/perform_pre_production_checks.py
+++ src/perform_pre_production_checks.py
@@ -25,7 +25,7 @@ def perform_pre_production_checks(row_num: int, form: str, parish: str, filename
     """
 
     parish_number = parish.split()[0]
-    RGX_FILENAMEPATTERN_FORM = re.compile(r"""^MAF32-(?P<box_number>\d+)[-_](?P<parish_number>\d+)_+(?P<image_number>0*\d+)\.tif$""")
+    RGX_FILENAMEPATTERN_FORM = re.compile(r"""^MAF32-(?P<box_number>\d+)[-_](?P<parish_number>\d+)_+(?P<image_number>\d+)\.tif$""")
     RGX_FILENAMEPATTERN_COVER = re.compile(r"""^MAF32-(?P<box_number>\d+)[-_](?P<parish_number>\d+)\.tif$""")
     RGX_FILENAMEPATTERN_OTHER = re.compile(r"""^MAF_*.*?\.tif$""")
 
