3562d607fcf419504e939b000638dd144f98c9b2
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-19 16:57:49 +0000

    strip raw data values

--- src/containers.py
+++ src/containers.py
@@ -72,7 +72,7 @@ class Farm:
         return [re.sub(r'"', "", item).strip() for item in re.split(r"; *", field_value)]
 
     def __init__(self, raw_farm_data: dict):
-        stripped_data = {key: value for key, value in raw_farm_data.items()}
+        stripped_data = {key: value.strip() for key, value in raw_farm_data.items()}
         """catalogue_reference & forms will have special methods to assign values later"""
         # self.catalogue_reference = make_catalogue_reference(stripped_data),
         # self.forms = assign_filenames_to_forms(stripped_data),
