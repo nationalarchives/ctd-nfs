ed1493d23255cee414fd0adf6d293d4f33c9c5bb
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-24 14:55:36 +0000

    refector method to use regex match containers

--- src/pre_instantiation_checks.py
+++ src/pre_instantiation_checks.py
@@ -36,11 +36,11 @@ def perform_pre_instantiation_checks(csv_values: dict) -> str | dict:
     }
     warnings: dict = make_warnings_mapping()
 
-    if not filename1_match:
+    if not pattern_matches['filename1']:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename1']} does not match expected pattern for form images. " \
                                               f"Further checks on filenames could not be carried out and an accurate reference could not be generated."})
 
-    if csv_values['filename2'] and not filename2_match:
+    if csv_values['filename2'] and not pattern_matches['filename2']:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename2']} does not match expected pattern for form images. " \
                                               f"Further checks on filenames could not be carried out and an accurate reference could not be generated."})
     
@@ -70,7 +70,7 @@ def perform_pre_instantiation_checks(csv_values: dict) -> str | dict:
     return "Pass"
 
 
-def check_filename_parts(csv_values: dict, filename1_match: re.Match, filename2_match: re.Match, warnings: dict) -> dict:
+def check_values_between_filenames(csv_values: dict, pattern_matches: dict[re.Match], warnings: dict) -> dict:
     """
     Performs checks on the box numbers, parish numbers and image numbers of the two file names
     * box numbers should be the same in the both file name
@@ -79,34 +79,38 @@ def check_filename_parts(csv_values: dict, filename1_match: re.Match, filename2_
 
     Args:
         csv_values (dict): dictionary with the following keys
-            row_num (int): row number from original csv, used for reporting errors/warning
-            form (str): form number from spreadsheet row
-            parish (str): parish number and name e.g. "1 Alkington"
-            filename1 (str): front page of form
-            filename2 (str, optional): back page of form Defaults to None, not used if form is Cover 
-        filename1_match (re.Match): 
-        filename2_match (re.Match): 
+            'row_num' (int): row number from original csv, used for reporting errors/warning
+            'form' (str): form number from spreadsheet row
+            'parish' (str): parish number and name e.g. "1 Alkington"
+            'filename1' (str): front page of form
+            'filename2' (str, optional): back page of form Defaults to None, not used if form is Cover 
+
+        pattern_matches
+            'filename1' (re.Match): match for filename1 against form pattern
+            'filename2' (re.Match): match for filename2 against form pattern
+            'cover' (re.Match): match for filename1 against cover pattern
+
         warnings (dict): warning messages for any issues found
 
     Returns:
         warnings (dict):
     """
 
-    if filename1_match['box_number'] != filename2_match['box_number']:
+    if pattern_matches['filename1']['box_number'] != pattern_matches['filename2']['box_number']:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename1']} and {csv_values['filename2']} have different box numbers. " \
                                                   f"Box number of {csv_values['filename1']} will be used in reference."})
 
     parish_number = csv_values['parish'].split()[0]
-    if filename1_match['parish_number'] != filename2_match['parish_number']:
+    if pattern_matches['filename1']['parish_number'] != pattern_matches['filename2']['parish_number']:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename1']} and {csv_values['filename2']} have different parish numbers. " \
                                                   f"Number from full parish name: '{csv_values['parish']}' will be used in catalogue reference."})
 
-    elif filename1_match['parish_number'] != parish_number:
+    elif pattern_matches['filename1']['parish_number'] != parish_number:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename1']} and {csv_values['filename2']} do not match value from parish name: '{parish_number}'. " \
                                                   f"Number from full parish name: '{csv_values['parish']}' will be used in catalogue reference."})
        
-    image1 = int(filename1_match['image_number'])
-    image2 = int(filename2_match['image_number'])
+    image1 = int(pattern_matches['filename1']['image_number'])
+    image2 = int(pattern_matches['filename2']['image_number'])
     if image2 != image1 + 1:
         warnings['Filename Warnings'].append({f"Row {csv_values['row_num']}": f"{csv_values['filename1']} and {csv_values['filename2']} are not consecutive images."})
 
