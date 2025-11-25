dae7a5a3c2d0f9622297a584438bbc2d2f351b0d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-25 16:58:57 +0000

    add test for pre_instatiation_checks

--- tests/unit/test_Farm_class.py
+++ tests/unit/test_Farm_class.py
@@ -1,8 +1,31 @@
-from src.containers import Farm
+from src.farm_class_setup import Farm
+from src.pre_instantiation_checks import perform_pre_instantiation_checks
 
 
 def test_farm_dataclass_instantiation(farms):
     for farm_fixture in farms:
         new_farm = Farm(farm_fixture)        
         assert isinstance(new_farm, Farm)
+
+
+def test_pre_instantiation_checks_pass(bad_farm_initial_values):
+	# item = pre_instantiation_checks_passed(bad_farm_initial_values)
+	for test in bad_farm_initial_values:
+		result = perform_pre_instantiation_checks(test['data'])
+		assert test['warning'] == result['Filename Warnings'][0]
+            
+
+# def pre_instantiation_checks_passed(farms):
+	# results = []
+	# for farm in farms:
+	# 	csv_values = {
+	# 		'row_num': farms.index(farm) + 1,
+	# 		'form': farm['document_type'],
+	# 		'parish': farm['parish'],
+	# 		'filename1': farm['filename_1'],
+	# 		'filename2': farm['filename_2']
+	# 	}
+	# 	result = perform_pre_instantiation_checks(csv_values)
+	# 	results.append(result)
+	# return results
 
