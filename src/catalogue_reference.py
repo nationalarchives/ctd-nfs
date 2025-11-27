007fb57aef6bd7df4b4f17850600e6e05d945561
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-11-27 17:31:40 +0000

    refactor method using one parameter
    new method uses one parameter containing all required values
    The original process from are outlined in the comments

--- src/catalogue_reference.py
+++ src/catalogue_reference.py
@@ -1,55 +1,88 @@
-def generate_references(filenames: list[str], primary_farm_number: str, additional_farms: str, form: str, row_num: int, existing_refs: list[str]) -> tuple[dict[str, str], set[str]]:
+def make_catalogue_reference(reference_data: dict[str]) -> str:
     """
-    Creates a catalogue reference for each farm in the required format: "MAF 32/<box number>/<parish number>/<farm number>"
+    Creates a catalogue reference for each farm in the required csv_data['document_type']at: f"MAF 32/<box number>/<parish number>/<farm number>"
     box number and parish number will be parsed from filename
     If reference can't be created due to missing data, a warning is added to the warnings set
+    •	The full catalogue reference as will be displayed in the catalogue
+    •	There can only be one catalogue reference per farm. In some instances, this may not be the case. See warnings and checks for more information.
+    Must begin “MAF 32”. See Overview of Catalogue structure for example. The piece/box number should be able to be extracted from the filename(s) in column A for most cases but not all.
 
     Args:
-        filenames (list[str]): box number parsed from filename
-        primary_farm_number (str): usually a number, can also be number/number e.g. "16/1", or "*"
-        additional_farms (str): identifier of additional farms, semi-colon separated if multiple
-        form (str): one of 'C51/SSY', 'B496/EI', 'C 47/SSY', 'C 49/SSY', 'SF', 'SF C69/SSY', 'Other', 'Cover'
-        row_num (int): row number in the source spreadsheet
-        existing_refs (list[str]): 
+        reference_data (dict[str]): _description_
 
     Returns:
-        tuple[dict[str, str], set[str]]: a list of generated references and a list of warnings
+        str: _description_
     """
     
-    ref_list = {}
+    # ref_list = {}
     warnings = set()
     
-    if primary_farm_number == "*":
-        ref = generate_ref("MAF 32/" + box_number + "/" + form, existing_refs)
-        ref_list[ref] = form
-        
-    elif primary_farm_number != "" and additional_farms == "":
-        ref = generate_ref("MAF 32/" + box_number + "/" + primary_farm_number, existing_refs)           
-        ref_list[ref] = "Primary"
-       
-    elif primary_farm_number == "" and additional_farms != "":
-        for additional_farm in additional_farms.split(";"):
-            ref = generate_ref("MAF 32/" + box_number + "/" + additional_farm.strip(), existing_refs)           
-            ref_list[ref] = "Additional"
-        warnings.add("Row " + row_num + ": Error - Additional farm but no primary farm given")
-        
-    elif primary_farm_number != "" and additional_farms != "":
-        ref = generate_ref("MAF 32/" + box_number + "/" + primary_farm_number, existing_refs)           
-        ref_list[ref] = "Primary"
-        
-        for additional_farm in additional_farms.split(";"):
-            ref = generate_ref("MAF 32/" + box_number + "/" + additional_farm.strip(), existing_refs)           
-            ref_list[ref] = "Additional"          
+    # if primary_farm_number is "*":
+    #   add document type to catalogue reference
+    #   map catalogue reference to document type
+    if reference_data['primary_farm_number'] == "*":
+        reference = reference_data['document_type']
+        # ref_list[ref] = csv_data['document_type']
+
+    # else if primary farm number is given and no additional farms: 
+    #   add primary farm number to catalogue reference
+    #   map catalogue_reference to "Primary"
+    elif reference_data['primary_farm_number'] and not reference_data['additional_farms']:
+        reference = reference_data['primary_farm_number']
+        # ref_list[ref] = "Primary"
+
+    # else if no primary farm number but additional farms:
+    #   for each additional farm:
+    #       add additional farm to catalogue reference
+    #       map catalogue reference to "Additional"
+    #   add warning "Error - Additional farm but no primary farm given
+    elif not reference_data['primary_farm_number'] and reference_data['additional_farms']:
+        for additional_farm in reference_data['additional_farms'].split(";"):
+            reference = additional_farm
+            # ref_list[ref] = "Additional"
+
+        warnings.add(f"Row {reference_data['row_num']}: Error - Additional farm but no primary farm given")
+
+    # else if both primary farm number and additional farms:
+    #   add primary farm number to catalogue reference
+    #   map catalogue reference to "Primary"        
+    #   for each additional farm:
+    #       add additional farm to catalogue reference
+    #       map catalogue reference to "Additional"
+    #   add warning "Warnings - Additional farms present"
+    elif reference_data['primary_farm_number'] and reference_data['additional_farms']:
+        reference = reference_data['primary_farm_number']
+        # ref_list[ref] = "Primary"
+
+        for additional_farm in reference_data['additional_farms'].split(";"):
+            reference = additional_farm
+            # ref_list[ref] = "Additional"
+        warnings.add(f"Row {reference_data['row_num']}: Warning - Additional farms present")
+    
+    # else if document_type is not 'Cover' or 'Other':
+    #   add warning - "Note - type is document_type  so no farm number specified"
+    #   add document type to catalogue reference
+    #   map catalogue reference to document_type
+    elif reference_data['document_type'] not in ["Other", "Cover"]:        
+        warnings.add(f"Row {reference_data['row_num']}: Note - type is {reference_data['document_type'].lower()} so no farm number specified")
+        reference = reference_data['document_type']
+        # ref_list[ref] = csv_data['document_type']
+
+
+    # else if document_type is 'Cover' and primary farm number is given:
+    #   add warning - "Error - Type is cover and farm number is specified"
+    #   add document type to catalogue reference
+    #   map catalogue reference to document_type
+    if reference_data['document_type'] == "Cover" and reference_data['primary_farm_number'] != "*":
+        warnings.add(f"Row {reference_data['row_num']}: Error - Type is cover and farm number is specified")
+        reference = reference_data['document_type']
+        # ref_list[ref] = csv_data['document_type']
+
                
-        warnings.add("Row " + row_num + ": Warning - Additional farms present")
-    elif form != "Other" or form != "Cover":        
-        warnings.add("Row " + row_num + ": Note - type is " + form.lower() + " so no farm number specified")
-        ref = generate_ref("MAF 32/" + box_number + "/" + form, existing_refs)
-        ref_list[ref] = form
-   
-    if type == "Cover" and primary_farm_number != "*":
-        warnings.add("Row " + row_num + ": Error - Type is cover and farm number is specified")
-        ref = generate_ref("MAF 32/" + box_number + "/" + form, existing_refs)
-        ref_list[ref] = form
-               
-    return (ref_list, warnings)  
+    catalogue_reference = \
+        f"MAF 32/" \
+        f"{reference_data['box_number']}/" \
+        f"{reference_data['parish_number']}/" \
+        f"{reference}"
+
+    return (catalogue_reference, warnings)
