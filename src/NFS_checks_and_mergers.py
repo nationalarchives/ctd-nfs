ef5835c11954d2d41ceef52879e1e2b2a73d86c5
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-04 14:03:58 +0000

    rename row_num
    no need to for abbreviated key name

--- src/NFS_checks_and_mergers.py
+++ src/NFS_checks_and_mergers.py
@@ -1,25 +1,63 @@
-from containers import make_warnings_mapping, make_forms_mapping, Farm
+from pathlib import Path
+import csv
+from typing import Generator
+
+from farm_class_setup import clean_csv_data, Farm
 from pre_instantiation_checks import perform_pre_instantiation_checks
 
 
-def check_and_instantiate_farm(raw_farm_data: dict) -> Farm:
-    """
-    Perform pre-instantiation checks on raw farm data and instantiate a Farm object if checks pass.
-    If checks fail, raise ValueError with appropriate message.
+CSV_FILES = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Samples")
+
+
+def load_data_from_file(csv_file: Path) -> list[dict]:
+    """_summary_
 
     Args:
-        raw_farm_data (dict): dictionary containing raw farm data from CSV
+        csv_file (Path):  
 
     Returns:
-        Farm: instantiated Farm object
+        dict: _description_
     """
     
-    warnings = perform_pre_instantiation_checks(stripped_data)
+    try:
+        print(f"Processing file: {csv_file.stem}")
 
-    farm = Farm(stripped_data)
+        with open(csv_file, newline='') as file_obj:
+            raw_csv_data = csv.DictReader(file_obj, skipinitialspace=True) 
+            cleaned_data: list[dict] = clean_csv_data(raw_csv_data)
+    except csv.Error as csv_error_message:
+        print(f"!!! ERROR in data loading: {csv_error_message}")
 
-    if warnings != "Pass":
-        raise ValueError(f"Pre-instantiation checks failed with warnings: {warnings}")
+    return cleaned_data
 
-    return farm
 
+def convert_to_generator(csv_data: list[dict]) -> Generator[dict, None, None]:
+    """_summary_
+
+    Args:
+        csv_data (list[dict]): _description_
+
+    Yields:
+        dict: _description_
+    """
+    for index, row in enumerate(csv_data):
+        row['row_number'] = index
+        yield row
+
+if __name__ == "__main__":
+    csv_files = CSV_FILES.glob("*.csv")
+
+    for csv_file in csv_files:
+        if csv_file.name != "Rutland Edited Data for RALPH.csv":
+            continue
+        try:
+            cleaned_farm_data: list[dict] = load_data_from_file(csv_file)
+            farm_data_generator = convert_to_generator(cleaned_farm_data)
+            for farm_data_row in farm_data_generator:
+                print(f"\nProcessing row {farm_data_row['row_number']} of file {csv_file.stem}")
+                reference_values, warnings = perform_pre_instantiation_checks(farm_data_row)
+                farm = Farm(farm_data_row)
+                print(f"Successfully instantiated Farm: {farm.county=}")
+
+        except ValueError as value_error_msg:
+            print(f"Failed to instantiate Farm from data: {value_error_msg}")
--- src/pre_instantiation_checks.py
+++ src/pre_instantiation_checks.py
@@ -1,6 +1,6 @@
 import re
 
-from src.farm_class_setup import make_warnings_mapping, make_forms_mapping
+from farm_class_setup import make_warnings_mapping, make_forms_mapping
 
 
 def perform_pre_instantiation_checks(csv_values: dict) -> tuple[dict, dict]:
@@ -16,7 +16,7 @@ def perform_pre_instantiation_checks(csv_values: dict) -> tuple[dict, dict]:
     
     Args:
         csv_values (dict): dictionary with the following keys
-            row_num (int): row number from original csv, used for reporting errors/warning
+            row_number (int): row number from original csv, used for reporting errors/warning
             document_type (str): form number from spreadsheet row
             parish (str): parish number and name e.g. "1 Alkington"
             filename_1 (str): front page of form
@@ -39,16 +39,16 @@ def perform_pre_instantiation_checks(csv_values: dict) -> tuple[dict, dict]:
     warnings: dict = make_warnings_mapping()
 
     if csv_values['document_type'] not in make_forms_mapping():
-        warnings['Type Warnings'].append(f"Row {csv_values['row_num']}: Form type '{csv_values['document_type']}' is not a recognised form.")
+        warnings['Type Warnings'].append(f"Row {csv_values['row_number']}: Form type '{csv_values['document_type']}' is not a recognised form.")
         return (reference_values, warnings)
 
     if not (pattern_matches['filename_1'] or pattern_matches['cover']):
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} does not match expected pattern for form images or cover. " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} does not match expected pattern for form images or cover. " \
                                               f"Further checks on filenames could not be carried out and an accurate reference could not be generated.")
         return (reference_values, warnings)
 
     if csv_values['filename_2'] and not pattern_matches['filename_2']:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_2']} does not match expected pattern for form images. " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_2']} does not match expected pattern for form images. " \
                                               f"Further checks on filenames could not be carried out and an accurate reference could not be generated.")
         return (reference_values, warnings)
     
@@ -78,7 +78,7 @@ def check_values_between_filenames(csv_values: dict, pattern_matches: dict[re.Ma
 
     Args:
         csv_values (dict): dictionary with the following keys
-            'row_num' (int): row number from original csv, used for reporting errors/warning
+            'row_number' (int): row number from original csv, used for reporting errors/warning
             'document_type' (str): form number from spreadsheet row
             'parish' (str): parish number and name e.g. "1 Alkington"
             'filename_1' (str): front page of form
@@ -96,22 +96,22 @@ def check_values_between_filenames(csv_values: dict, pattern_matches: dict[re.Ma
     """
 
     if pattern_matches['filename_1']['box_number'] != pattern_matches['filename_2']['box_number']:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} and {csv_values['filename_2']} have different box numbers. " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} and {csv_values['filename_2']} have different box numbers. " \
                                                   f"Box number of {csv_values['filename_1']} will be used in the catalogue reference.")
 
     parish_number = csv_values['parish'].split()[0]
     if pattern_matches['filename_1']['parish_number'] != pattern_matches['filename_2']['parish_number']:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} and {csv_values['filename_2']} have different parish numbers. " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} and {csv_values['filename_2']} have different parish numbers. " \
                                                   f"Parish number from full parish name '{csv_values['parish']}' will be used in the catalogue reference.")
 
     elif pattern_matches['filename_1']['parish_number'] != parish_number:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} and {csv_values['filename_2']} have a diffeent parish number from parish name '{csv_values['parish']}'. " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} and {csv_values['filename_2']} have a diffeent parish number from parish name '{csv_values['parish']}'. " \
                                                   f"Parish number from full parish name will be used in the catalogue reference.")
        
     image1 = int(pattern_matches['filename_1']['image_number'])
     image2 = int(pattern_matches['filename_2']['image_number'])
     if image2 != image1 + 1:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} and {csv_values['filename_2']} are either not consecutive images or in the wrong order.")
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} and {csv_values['filename_2']} are either not consecutive images or in the wrong order.")
     
     return warnings
 
@@ -124,7 +124,7 @@ def check_cover_image_consistency(csv_values: dict, pattern_matches: dict[re.Mat
 
     Args:
         csv_values (dict): dictionary with the following keys
-            row_num (int): row number from original csv, used for reporting errors/warning
+            row_number (int): row number from original csv, used for reporting errors/warning
             form (str): form number from spreadsheet row
             parish (str): parish number and name e.g. "1 Alkington"
             filename_1 (str): front page of form
@@ -145,20 +145,20 @@ def check_cover_image_consistency(csv_values: dict, pattern_matches: dict[re.Mat
     images_are_for_document_which_is_not_cover = pattern_matches['filename_1'] and pattern_matches['filename_1']['image_number'] != "0001" and pattern_matches['filename_2']
     
     if document_type_is_cover and not file_is_cover_image:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: Form type is 'Cover' but {csv_values['filename_1']} does not match expected cover pattern or have image number 0001.")
-        warnings['Type Warnings'].append(f"Row {csv_values['row_num']}: [see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: Form type is 'Cover' but {csv_values['filename_1']} does not match expected cover pattern or have image number 0001.")
+        warnings['Type Warnings'].append(f"Row {csv_values['row_number']}: [see Filename Warnings]")
 
     if not document_type_is_cover and file_is_cover_image:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: {csv_values['filename_1']} matches expected cover pattern or has image number 0001 but form type is '{csv_values['document_type']}'.")
-        warnings['Type Warnings'].append(f"Row {csv_values['row_num']}: [see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: {csv_values['filename_1']} matches expected cover pattern or has image number 0001 but form type is '{csv_values['document_type']}'.")
+        warnings['Type Warnings'].append(f"Row {csv_values['row_number']}: [see Filename Warnings]")
 
     if document_type_is_cover and file_is_cover_image and csv_values['filename_2']:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: Form type is 'Cover', and {csv_values['filename_1']} matches expected pattern for cover image " \
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: Form type is 'Cover', and {csv_values['filename_1']} matches expected pattern for cover image " \
                                              f"but additional image {csv_values['filename_2']} was also provided.")
-        warnings['Type Warnings'].append(f"Row {csv_values['row_num']}: document is listed as 'Cover' in data but two form images provided.")
+        warnings['Type Warnings'].append(f"Row {csv_values['row_number']}: document is listed as 'Cover' in data but two form images provided.")
 
     if images_are_for_document_which_is_not_cover and document_type_is_cover:
-        warnings['Filename Warnings'].append(f"Row {csv_values['row_num']}: Form type is 'Cover' but two form images were provided: {csv_values['filename_1']} and {csv_values['filename_2']}.")
-        warnings['Type Warnings'].append(f"Row {csv_values['row_num']}: [see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"Row {csv_values['row_number']}: Form type is 'Cover' but two form images were provided: {csv_values['filename_1']} and {csv_values['filename_2']}.")
+        warnings['Type Warnings'].append(f"Row {csv_values['row_number']}: [see Filename Warnings]")
     
     return warnings
