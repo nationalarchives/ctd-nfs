4346cc142e7fb265c12a8c6f560a1ce259ee0ffc
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-16 21:17:42 +0000

    add lookup table constant

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,9 @@ module for defining constants and constant namespaces.
 """
 
 from pathlib import Path
+import shelve
+
+from read_db_cache import LOOKUP_TABLE_DB
 
 
 EXCEL_LOOKUP_FILE = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\MAF 32 Piece Lookup Table 04-12-2025.xlsx")
@@ -13,7 +16,10 @@ class DataFolders():
     __slots__ = ()
 
     _root = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Pipeline")
-    LOOKUP_TABLE = _root / "0-DB" / DB_FILE
+    
+    _db = _root / "0-DB" / DB_FILE
+    with shelve.open(_db, "c") as shelf:
+        LOOKUP_TABLE = shelf['pieces lookup table']    
     INPUT = _root / "1-INPUT"
     TRANSFORM = _root / "3-TRANSFORM"
     ARCHIVE = _root / "4-ARCHIVE"
