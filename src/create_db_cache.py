cd989a114b1b6f6a75a0a9f97bc17b6be8958644
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-16 20:29:06 +0000

    add module to create look table db
    The lookup table will be used to find the catalogue reference

---
+++ src/create_db_cache.py
@@ -0,0 +1,41 @@
+"""
+    Creates a shelve database from an excel lookup table
+
+Returns:
+    _type_: _description_
+"""
+from pathlib import Path
+import shelve
+
+from xlreader import read_file
+
+from constants import DATA, EXCEL_LOOKUP_FILE
+
+
+def read_records_from_file(excel_file: Path) -> list[dict]:  
+    """
+    Reads data from file as a dictionary where each {key, value} refers to one sheet and its rows (a list of tuples) 
+    Returns a list of dictionaries, using the first row as the header
+
+    Args:
+        excel_file (Path):
+
+    Returns:
+        list[dict]: 
+    """
+    file_data = read_file(excel_file)
+    sheet_name = list(file_data.keys())[0]
+    data_rows = file_data[sheet_name]
+    return [
+        dict(zip(data_rows[0], row))
+        for row in data_rows[1:]
+        if row[1]
+    ]
+
+
+if __name__ == "__main__":
+
+    lookup_table: list[dict] = read_records_from_file(EXCEL_LOOKUP_FILE)
+    with shelve.open(DATA.LOOKUP_TABLE) as shelf:
+        shelf['pieces lookup table'] = lookup_table
+
