c85e6d9976149859806c00986d8ca1d867a615ca
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-18 10:41:33 +0000

    change get_references function to use combined county and parish values
    'Parish Number' value is an integer, so this is an alternative to reference['Parish Number'] == int(parish_number) - it means a less verbose test

--- src/farm_class_setup.py
+++ src/farm_class_setup.py
@@ -58,7 +58,7 @@ def get_references(county_code: str, parish_number: str) -> tuple:
     all_references = (
         reference
         for reference in DATA.LOOKUP_TABLE
-        if reference['County Code'] == county_code and reference['Parish Number'] == parish_number
+        if reference['County & Parish'] == f"{county_code}/{parish_number}"
     )
     reference_record = next(all_references, None)
     
