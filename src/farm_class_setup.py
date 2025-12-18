b7d9455d129228fa04c67c41a19ece2e2abf988a
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-18 11:20:27 +0000

    format code

--- src/farm_class_setup.py
+++ src/farm_class_setup.py
@@ -97,7 +97,6 @@ class Farm:
     catalogue_reference: str
     farm_reference: str
     forms: OrderedDict[str, list[str]]
-    warnings: dict[str, list[str]]
 
     def __init__(self, cleaned_csv_data: dict):
         self.county = cleaned_csv_data['county']
