aa3b51ba408da1e60d0a552f22a1e63459fb27bd
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2025-12-21 22:08:45 +0000

    rename method

--- src/NFS_checks_and_mergers.py
+++ src/NFS_checks_and_mergers.py
@@ -1,48 +1,14 @@
 from pathlib import Path
-import csv
-from typing import Generator
+import re
 
-from farm_class_setup import clean_csv_data, Farm
-from pre_instantiation_checks import perform_pre_instantiation_checks
-
-
-CSV_FILES = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Samples")
+from farm_class_setup import Farm
+from pre_instantiation_checks import validate_farm_reference_values
+from constants import DATA, REGEX
+from src.file_processor import convert_to_generator, load_data_from_file
 
 
-def load_data_from_file(csv_file: Path) -> list[dict]:
-    """_summary_
+CSV_FILES = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Pipeline\1-INPUT")
 
-    Args:
-        csv_file (Path):  
-
-    Returns:
-        dict: _description_
-    """
-    
-    try:
-        print(f"Processing file: {csv_file.stem}")
-
-        with open(csv_file, newline='') as file_obj:
-            raw_csv_data = csv.DictReader(file_obj, skipinitialspace=True) 
-            cleaned_data: list[dict] = clean_csv_data(raw_csv_data)
-    except csv.Error as csv_error_message:
-        print(f"!!! ERROR in data loading: {csv_error_message}")
-
-    return cleaned_data
-
-
-def convert_to_generator(csv_data: list[dict]) -> Generator[dict, None, None]:
-    """_summary_
-
-    Args:
-        csv_data (list[dict]): _description_
-
-    Yields:
-        dict: _description_
-    """
-    for index, row in enumerate(csv_data):
-        row['row_number'] = index
-        yield row
 
 if __name__ == "__main__":
     csv_files = CSV_FILES.glob("*.csv")
@@ -50,14 +16,33 @@ if __name__ == "__main__":
     for csv_file in csv_files:
         if csv_file.name != "Rutland Edited Data for RALPH.csv":
             continue
-        try:
-            cleaned_farm_data: list[dict] = load_data_from_file(csv_file)
-            farm_data_generator = convert_to_generator(cleaned_farm_data)
-            for farm_data_row in farm_data_generator:
-                print(f"\nProcessing row {farm_data_row['row_number']} of file {csv_file.stem}")
-                reference_values, warnings = perform_pre_instantiation_checks(farm_data_row)
-                farm = Farm(farm_data_row)
-                print(f"Successfully instantiated Farm: {farm.county=}")
+        cleaned_farm_data: list[dict] = load_data_from_file(csv_file)
+        farm_data_generator = convert_to_generator(cleaned_farm_data)
+        for farm_data_row in farm_data_generator:
+            print(f"\nProcessing row {farm_data_row['row_number']} ...")
+            pattern_matches: dict[re.Match] = {
+                'filename_1': REGEX.FORM_PATTERN.match(farm_data_row['filename_1']),
+                'filename_2': REGEX.FORM_PATTERN.match(farm_data_row['filename_2']),
+                'cover': REGEX.COVER_PATTERN.match(farm_data_row['filename_1']),
+            }
+            row_prefix = f"Row {farm_data_row['row_number']}: "
 
-        except ValueError as value_error_msg:
-            print(f"Failed to instantiate Farm from data: {value_error_msg}")
+            if not validate_farm_reference_values(farm_data_row, pattern_matches):
+                continue
+
+                warnings = perform_pre_instantiation_checks(farm_data_row)
+            except Exception as check_exception_msg:
+                print(f"{check_exception_msg}")
+                continue
+
+            candidate_farm = Farm(**farm_data_row)
+            candidate_farm.warnings = warnings
+            if candidate_farm.catalogue_reference not in Farm.all_farms:
+                Farm.all_farms[candidate_farm.catalogue_reference] = candidate_farm
+                print(f"Successfully instantiated Farm: {candidate_farm.catalogue_reference}")
+            else:
+                print(f"Catalogue reference {candidate_farm.catalogue_reference} already exists. Merging data ...")
+                # existing_farm = Farm.all_farms[candidate_farm.catalogue_reference]
+                # existing_farm.merge_with_another_farm(candidate_farm)
+                # print(f"Successfully merged Farm: {candidate_farm.catalogue_reference}")
+        # print(f"{Farm.all_farms=}")
