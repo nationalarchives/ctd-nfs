72c88321f7163eb35e1d5de34347dfedf5a691c6
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-07 17:29:12 +0000

    fix: Use row_prefix parameter in message strings

--- src/pre_instantiation_checks.py
+++ src/pre_instantiation_checks.py
@@ -172,21 +172,21 @@ def report_cover_image_inconsistencies(csv_values: dict, pattern_matches: dict[r
         pattern_matches['filename_2']
 
     if document_type_is_cover and not file_is_cover_image:
-        warnings['Filename Warnings'].append(f"{row}Form type is 'Cover' but {csv_values['filename_1']} does not match expected cover pattern or have image number 0001.")
-        warnings['Type Warnings'].append(f"{row}[see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"{row_prefix}Form type is 'Cover' but {csv_values['filename_1']} does not match expected cover pattern or have image number 0001.")
+        warnings['Type Warnings'].append(f"{row_prefix}[see Filename Warnings]")
 
     if not document_type_is_cover and file_is_cover_image:
-        warnings['Filename Warnings'].append(f"{row}{csv_values['filename_1']} matches expected cover pattern or has image number 0001 but form type is '{csv_values['document_type']}'.")
-        warnings['Type Warnings'].append(f"{row}[see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"{row_prefix}{csv_values['filename_1']} matches expected cover pattern or has image number 0001 but form type is '{csv_values['document_type']}'.")
+        warnings['Type Warnings'].append(f"{row_prefix}[see Filename Warnings]")
 
     if document_type_is_cover and file_is_cover_image and csv_values['filename_2']:
-        warnings['Filename Warnings'].append(f"{row}Form type is 'Cover', and {csv_values['filename_1']} matches expected pattern for cover image " \
+        warnings['Filename Warnings'].append(f"{row_prefix}Form type is 'Cover', and {csv_values['filename_1']} matches expected pattern for cover image " \
                                              f"but additional image {csv_values['filename_2']} was also provided.")
-        warnings['Type Warnings'].append(f"{row}document is listed as 'Cover' in data but two form images provided.")
+        warnings['Type Warnings'].append(f"{row_prefix}document is listed as 'Cover' in data but two form images provided.")
 
     if images_are_for_document_which_is_not_cover and document_type_is_cover:
-        warnings['Filename Warnings'].append(f"{row}Form type is 'Cover' but two form images were provided: {csv_values['filename_1']} and {csv_values['filename_2']}.")
-        warnings['Type Warnings'].append(f"{row}[see Filename Warnings]")
+        warnings['Filename Warnings'].append(f"{row_prefix}Form type is 'Cover' but two form images were provided: {csv_values['filename_1']} and {csv_values['filename_2']}.")
+        warnings['Type Warnings'].append(f"{row_prefix}[see Filename Warnings]")
     
     return warnings
 
