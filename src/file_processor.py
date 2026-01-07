a1f5db0ccddd9065612f35aaff72a6496bc7721c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-07 17:27:32 +0000

    fix: Correct generator return

--- src/file_processor.py
+++ src/file_processor.py
@@ -44,7 +44,8 @@ def load_data_from_file(csv_file: Path) -> Generator[dict, None, None]:
         print(f"Processing file: {csv_file.stem}")
         with open(csv_file, newline='') as file_obj:
             raw_csv_data = csv.DictReader(file_obj, skipinitialspace=True)
-        return iter(raw_csv_data)
+            for row in raw_csv_data:
+                yield row
     
     except csv.Error as csv_error_message:
         print(f"!!! ERROR in data loading: {csv_error_message}")
