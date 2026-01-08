61485666c87f465b1e3dbdd7bcc560cf69c4f72b
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-08 21:01:22 +0000

    feat: parse county name from file name

--- src/NFS_checks_and_mergers.py
+++ src/NFS_checks_and_mergers.py
@@ -1,6 +1,6 @@
 import shelve
 
-from src._config.constants import DATA
+from src._config.constants import DATA, REGEX
 from src.file_processor import process_file
 
 
@@ -9,5 +9,6 @@ if __name__ == "__main__":
         county_names = piece_lookup_db['County names']
 
     for csv_file in DATA.INPUT.glob("*.csv"):
-        process_file(csv_file)
+        if REGEX.COUNTY_NAME.match(csv_file.name)["county_name"] in county_names:
+            process_file(csv_file)
         # print(f"{Farm.all_farms=}")
