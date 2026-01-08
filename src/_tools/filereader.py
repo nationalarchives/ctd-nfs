b35fff4fa95106a983d1cfc7496b8ffa0e1d56eb
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-08 13:44:25 +0000

    feat: add support to read multiple file types
    This module is copied from the tools libary of an old project, so includes methods for file types not used in this project at time of writing

---
+++ src/_tools/filereader.py
@@ -0,0 +1,30 @@
+from typing import Any, Union
+from importlib.resources import files, as_file
+from pathlib import Path
+import json
+import yaml
+
+from src._tools.xlreader import read_file
+
+
+def get_data_from_file(_data_file: Union[str, Path], _csv_fieldnames=None) -> Any:
+    if _data_file.suffix in [".xls", ".xlsx"]:
+        return read_file(_data_file)
+
+    if _data_file.suffix in [".log", ".txt", ".html"]:
+        return _data_file.read_text(encoding="utf-8")
+
+    with _data_file.open(mode="r", encoding="utf-8") as _stream:
+        if _data_file.suffix == ".yaml":
+            return yaml.safe_load(_stream)
+
+        if _data_file.suffix == ".json":
+            return json.load(_stream)
+
+
+def get_data_file_from_package(package: str, file_path_relative_to_package: str) -> Any:
+    data_file = files(package).joinpath(file_path_relative_to_package)
+    with as_file(data_file) as file_object:
+        return get_data_from_file(file_object)
+
+
