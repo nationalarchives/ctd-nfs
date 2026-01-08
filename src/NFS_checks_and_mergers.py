e2c34f3d52a3082c2e2c60fdd3355c2438ba8841
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-08 20:20:58 +0000

    feat: read in county names db

--- src/NFS_checks_and_mergers.py
+++ src/NFS_checks_and_mergers.py
@@ -3,6 +3,7 @@ import shelve
 from src._config.constants import DATA
 from src.file_processor import process_file
 
+
 if __name__ == "__main__":
     with shelve.open(DATA.PIECE_LOOKUP_TABLE, "r") as piece_lookup_db:   
         county_names = piece_lookup_db['County names']
