834c12efe773daf14527c75abefd86faa7377ca9
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-08 17:46:38 +0000

    feat: add regular expresssion to extract county name from input file name

--- src/_config/constants.py
+++ src/_config/constants.py
@@ -16,6 +16,7 @@ _FARMS_CACHE = Path("MAF 32 Farms.db")
 class RegexPatterns():
     __slots__ = ()
 
+    COUNTY_NAME = re.compile(r"""^(?P<county_name>[A-Z\s\(\)]+)+.*?$""")
     FORM_PATTERN = re.compile(r"""^MAF32-(?P<piece>\d+)[-_](?P<parish_number>\d+)_+(?P<image_number>\d+)\.tif$""")
     COVER_PATTERN = re.compile(r"""^MAF32-(?P<piece>\d+)[-_](?P<parish_number>\d+)\.tif$""")
 
