eec452bde485aad711178a6d3e84b96b7955a0ad
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-12 12:53:55 +0000

    refactor: move private constants into PathNamespace

--- src/_config/constants.py
+++ src/_config/constants.py
@@ -7,10 +7,6 @@ import re
 
 
 EXCEL_LOOKUP_FILE = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\MAF 32 Piece Lookup Table 04-12-2025.xlsx")
-_PIPELINE_ROOT = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Pipeline")
-    
-_PIECE_LOOKUP_DB = Path("MAF 32 Piece Lookup Table.db")
-_FARMS_CACHE = Path("MAF 32 Farms.db")
 
 
 class RegexNamespace():
@@ -23,6 +19,10 @@ class RegexNamespace():
 class PathNamespace():
     __slots__ = ()
     
+    _PIPELINE_ROOT = Path(r"C:\Users\rbruno\OneDrive - The National Archives\Projects\NFS\Pipeline")
+    _PIECE_LOOKUP_DB = Path("MAF 32 Piece Lookup Table.db")
+    _FARMS_CACHE = Path("MAF 32 Farms.db")
+
     PIECE_LOOKUP_TABLE = _PIPELINE_ROOT / "0-DB" / _PIECE_LOOKUP_DB
     FARMS_DB = _PIPELINE_ROOT / "0-DB" / _FARMS_CACHE
     INPUT = _PIPELINE_ROOT / "1-INPUT"
