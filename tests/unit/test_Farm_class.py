6fbb036bf75c91ae9252cbe94aa012f9f9cb8f1b
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-16 14:19:20 +0000

    tests: add assertions for forms concatenation

--- tests/unit/test_Farm_class.py
+++ tests/unit/test_Farm_class.py
@@ -31,14 +31,20 @@ def test_farm_attribute_concatenation(concatenation_data):
 		new_farm = Farm(**test_data['data'][1])
 		concatenated_farm = concatenate_farms(existing_farm, new_farm)
 		if farm_name == "10 Burley/7":
-			assert concatenated_farm.farm_name == test_data['result']
+			assert concatenated_farm.farm_name == test_data['result']['farm_name']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "49 Tickencote/6":
-			assert concatenated_farm.addressee.group_names == test_data['result']	
+			assert concatenated_farm.addressee.group_names == test_data['result']['addressee_group_names']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "96 Cockermouth/10":
-			assert concatenated_farm.addressee.group_names == test_data['result']
+			assert concatenated_farm.addressee.group_names == test_data['result']['addressee_group_names']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "97 Dean/47":
-			assert concatenated_farm.addressee.title == test_data['result']
+			assert concatenated_farm.addressee.title == test_data['result']['addressee_title']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "296 Farmborough/14":
-			assert concatenated_farm.farm_name == test_data['result']
+			assert concatenated_farm.farm_name == test_data['result']['farm_name']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "91 Dulverton/26":
-			assert concatenated_farm.owner.group_names == test_data['result']
+			assert concatenated_farm.owner.group_names == test_data['result']['owner_group_names']
+			assert dict(concatenated_farm.forms) == test_data['result']['forms']
