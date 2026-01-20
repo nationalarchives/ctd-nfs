a08200f844fed60854955bd02af8dde54ff4ff25
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-20 23:17:40 +0000

    feat: define custom exception

---
+++ src/_config/custom_exceptions.py
@@ -0,0 +1,7 @@
+class FileNamePatternError(Exception):   
+    """Custom exception for filename pattern errors."""
+    pass   
+
+class FormValidationError(Exception):   
+    """Custom exception for form validation errors."""
+    pass
