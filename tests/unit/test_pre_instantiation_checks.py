2b53c032357e17048c09d66da5d4f4896416408d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-22 12:08:53 +0000

    test: change row_prefix  so that matches pre_instantiation_checks

--- tests/unit/test_pre_instantiation_checks.py
+++ tests/unit/test_pre_instantiation_checks.py
@@ -16,7 +16,7 @@ def setup(test_data, row_num) -> tuple:
 		'filename_2': REGEX.FORM_PATTERN.match(test_data['filename_2']),
 		'cover': REGEX.COVER_PATTERN.match(test_data['filename_1']),
 	}
-	row_prefix = f"Row {row_num}"
+	row_prefix = f"Row {row_num}: "
 
 	return pattern_matches, row_prefix
 
