e418266f21ec9bd203452b219de6188f30e90c0c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-25 22:12:00 +0000

    test[chore]: print out dates to confirm normalization

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -5,7 +5,6 @@ from src._config.constants import PATH
 from src.file_processor import process_file
 from src.farm_builder import Farm
 
-
 def test_nfs_checks_and_mergers():
     for csv_file in PATH.TEST_INPUT.glob("*.csv"):
         process_file(csv_file, test_mode=True)
@@ -14,5 +13,6 @@ def test_nfs_checks_and_mergers():
         for county in test_db.keys():
             for farm in test_db[county].keys():
                 assert isinstance(test_db[county][farm], Farm)
+                print(f"\t{test_db[county][farm].field_info_date} -> {test_db[county][farm].primary_record_date}")
 
     os.remove(PATH.TEST_DB)
