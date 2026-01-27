3954bd8a60324842c07b0fa20414b384e235a955
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-27 10:42:09 +0000

    feat: reduce format possibilities in date parameter

--- src/farm_builder.py
+++ src/farm_builder.py
@@ -76,6 +76,7 @@ def normalize_date(candi_date: str) -> str:
         str: The normalized date string.
     """
 
+    candi_date = re.sub(r'[/-. ]+', ' ', candi_date)
     date_match: dict[re.Match] = {
         'daymonthyear': REGEX.DAYMONTHYEAR.match(candi_date),
         'ddmmyyyy': REGEX.DDMMYYYY.match(candi_date),
