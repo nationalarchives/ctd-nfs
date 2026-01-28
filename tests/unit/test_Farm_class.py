aea17b2a6275c4dfe3226d67d3e481b036af4ddc
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-28 17:59:08 +0000

    test: new test specifically for form concatenation

--- tests/unit/test_Farm_class.py
+++ tests/unit/test_Farm_class.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.farm_builder import Farm, concatenate_farms
+from src.farm_builder import Farm, concatenate_farms, concatenate_forms
 from src.farm_builder import normalize_date
 
 @pytest.mark.skip
@@ -20,24 +20,32 @@ def test_farm_attribute_concatenation(concatenation_data):
 		concatenated_farm = concatenate_farms(existing_farm, new_farm)
 		if farm_name == "10 Burley/7":
 			assert concatenated_farm.farm_name == test_data['result']['farm_name']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "49 Tickencote/6":
 			assert concatenated_farm.addressee.group_names == test_data['result']['addressee_group_names']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "96 Cockermouth/10":
 			assert concatenated_farm.addressee.group_names == test_data['result']['addressee_group_names']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "97 Dean/47":
 			assert concatenated_farm.addressee.title == test_data['result']['addressee_title']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "296 Farmborough/14":
 			assert concatenated_farm.farm_name == test_data['result']['farm_name']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 		if farm_name == "91 Dulverton/26":
 			assert concatenated_farm.owner.group_names == test_data['result']['owner_group_names']
-			assert dict(concatenated_farm.forms) == test_data['result']['forms']
 
 
+def test_concatenate_forms(concatenate_forms_fixture):
+	for index, farm_data in enumerate(concatenate_forms_fixture):
+		existing_farm = farm_data[0]
+		for new_farm in farm_data[1:]:
+			existing_farm = concatenate_forms(existing_farm, new_farm)
+		if index == 0:
+			assert len(existing_farm['B496/EI']) == 2
+			continue
+
+		if index == 1:
+			assert len(existing_farm['B496/EI'][0].images) == 3
+			continue
+
+@pytest.mark.skip
 def test_normalize_date(valid_dates, normalized_dates):
 	for index, test_date in enumerate(valid_dates['data']):
 		expected_result = normalized_dates[index]
