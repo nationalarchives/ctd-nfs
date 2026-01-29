d48932bec9c42275b21bf348eb4f9212740fe58f
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-29 16:23:20 +0000

    refactor: rename methods

--- tests/unit/test_reference_values.py
+++ tests/unit/test_reference_values.py
@@ -6,7 +6,7 @@ from tests.unit.conftest import setup
 import pytest
 
 
-def test_farm_reference_values_bad_form(farm_reference_values_bad_form):
+def test_reference_values_bad_form(farm_reference_values_bad_form):
 	pattern_matches, row_prefix = setup(
 		farm_reference_values_bad_form['data'],
 		farm_reference_values_bad_form['row_num']
@@ -16,7 +16,7 @@ def test_farm_reference_values_bad_form(farm_reference_values_bad_form):
 		validate_farm_reference_values(farm_reference_values_bad_form['data'], pattern_matches, row_prefix)
 
 
-def test_farm_reference_values_filename1_bad_pattern(farm_reference_values_filename1_bad_pattern):
+def test_reference_values_filename1_bad_pattern(farm_reference_values_filename1_bad_pattern):
 	pattern_matches, row_prefix = setup(
 		farm_reference_values_filename1_bad_pattern['data'],
 		farm_reference_values_filename1_bad_pattern['row_num']
@@ -26,7 +26,7 @@ def test_farm_reference_values_filename1_bad_pattern(farm_reference_values_filen
 		validate_farm_reference_values(farm_reference_values_filename1_bad_pattern['data'], pattern_matches, row_prefix)
 
 
-def test_farm_reference_values_filename2_bad_pattern(farm_reference_values_filename2_bad_pattern):
+def test_reference_values_filename2_bad_pattern(farm_reference_values_filename2_bad_pattern):
 	pattern_matches, row_prefix = setup(
 		farm_reference_values_filename2_bad_pattern['data'],
 		farm_reference_values_filename2_bad_pattern['row_num']
@@ -36,7 +36,7 @@ def test_farm_reference_values_filename2_bad_pattern(farm_reference_values_filen
 		validate_farm_reference_values(farm_reference_values_filename2_bad_pattern['data'], pattern_matches, row_prefix)
 
 
-def test_farm_reference_values_no_farm_data(farm_reference_values_no_farm_data):
+def test_reference_values_no_farm_data(farm_reference_values_no_farm_data):
 	pattern_matches, row_prefix = setup(
 		farm_reference_values_no_farm_data['data'],
 		farm_reference_values_no_farm_data['row_num']
