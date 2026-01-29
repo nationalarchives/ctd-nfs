cc25369205c367a70dc5df9574b1b0b979ae749a
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-29 16:17:41 +0000

    refactor: extract tests to separate module

---
+++ tests/unit/test_reference_values.py
@@ -0,0 +1,46 @@
+from _config.custom_exceptions import FileNamePatternError
+from row_data_validator import validate_farm_reference_values
+from tests.unit.conftest import setup
+
+
+import pytest
+
+
+def test_farm_reference_values_bad_form(farm_reference_values_bad_form):
+	pattern_matches, row_prefix = setup(
+		farm_reference_values_bad_form['data'],
+		farm_reference_values_bad_form['row_num']
+		)
+
+	with pytest.raises(FileNamePatternError, match=farm_reference_values_bad_form['error']):
+		validate_farm_reference_values(farm_reference_values_bad_form['data'], pattern_matches, row_prefix)
+
+
+def test_farm_reference_values_filename1_bad_pattern(farm_reference_values_filename1_bad_pattern):
+	pattern_matches, row_prefix = setup(
+		farm_reference_values_filename1_bad_pattern['data'],
+		farm_reference_values_filename1_bad_pattern['row_num']
+		)
+
+	with pytest.raises(FileNamePatternError, match=farm_reference_values_filename1_bad_pattern['error']):
+		validate_farm_reference_values(farm_reference_values_filename1_bad_pattern['data'], pattern_matches, row_prefix)
+
+
+def test_farm_reference_values_filename2_bad_pattern(farm_reference_values_filename2_bad_pattern):
+	pattern_matches, row_prefix = setup(
+		farm_reference_values_filename2_bad_pattern['data'],
+		farm_reference_values_filename2_bad_pattern['row_num']
+		)
+
+	with pytest.raises(FileNamePatternError, match=farm_reference_values_filename2_bad_pattern['error']):
+		validate_farm_reference_values(farm_reference_values_filename2_bad_pattern['data'], pattern_matches, row_prefix)
+
+
+def test_farm_reference_values_no_farm_data(farm_reference_values_no_farm_data):
+	pattern_matches, row_prefix = setup(
+		farm_reference_values_no_farm_data['data'],
+		farm_reference_values_no_farm_data['row_num']
+		)
+
+	with pytest.raises(FileNamePatternError, match=farm_reference_values_no_farm_data['error']):
+		validate_farm_reference_values(farm_reference_values_no_farm_data['data'], pattern_matches, row_prefix)
