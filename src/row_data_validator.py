b06c355c740806131b638c2339051d7af765466e
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-30 12:05:58 +0000

    feat: move cover condictional to helper function to improve clarity and flow

--- src/row_data_validator.py
+++ src/row_data_validator.py
@@ -169,15 +169,13 @@ def check_for_cover_with_farm_details(csv_values: dict, pattern_matches: dict[re
         warnings['Type Warnings'].append(f"{row_prefix}[see Filename Warnings]")
     
     return warnings 
-    
+
 
 def has_cover_issues(csv_values: dict, pattern_matches: dict[re.Match], row_prefix: str) -> dict | None:
     no_cover_warnings = initialise_warnings_mapping()
 
     warnings = check_for_cover_with_farm_details(csv_values, pattern_matches, no_cover_warnings, row_prefix)
-
-    if csv_values['document_type'] == 'Cover' or pattern_matches['cover'] or pattern_matches['filename_1']['image_number'] == "0001":
-        warnings = report_cover_image_inconsistencies(csv_values, pattern_matches, warnings, row_prefix)
+    warnings = report_cover_image_inconsistencies(csv_values, pattern_matches, warnings, row_prefix)
 
     if warnings != no_cover_warnings:
         return warnings
