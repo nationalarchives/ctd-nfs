2b13fa4a8f0a4b7efd510fb4bc601f3fdb02b2c3
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-01-30 14:29:17 +0000

    chore improve message formatting

--- src/file_processor.py
+++ src/file_processor.py
@@ -69,11 +69,11 @@ def update_farms_db(new_farm: Farm, row_number: int, test_mode: bool = False) ->
             logger.info(f"{row_info} NEW FARM: {new_farm.catalogue_reference}")
 
         else:
-            logger.info(f"{row_info} --- EXISTING CATALOGUE REFERENCE {new_farm.catalogue_reference} found.")
+            logger.info(f"{row_info}{' '*11}EXISTING CATALOGUE REFERENCE {new_farm.catalogue_reference} found.")
             existing_farm = county[new_farm.catalogue_reference]
             county.update({new_farm.catalogue_reference: concatenate_farms(existing_farm, new_farm)})
             farm_db[new_farm.county] = county
-            logger.info(f"{row_info} ---     Updated     ---")
+            logger.info(f"{row_info}{' '*11}{'Updated':.^35}")
 
 
 def create_farms(csv_data: Iterator[dict], test_mode: bool = False) -> None:
