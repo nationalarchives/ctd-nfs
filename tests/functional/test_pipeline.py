5fc504cf9c064993bf9df6713be2916b3872c94a
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-05 12:18:57 +0000

    feat:update test to use new database structure

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -14,8 +14,8 @@ def test_nfs_checks_and_mergers():
         process_file(csv_file, test_mode=True)
 
     with shelve.open(PATH.TEST_DB, 'r') as test_db:
-        for county in test_db.keys():
-            for farm in test_db[county].keys():
-                assert isinstance(test_db[county][farm], Farm)
-                print(f"\t{test_db[county][farm].field_info_date} -> {test_db[county][farm].primary_record_date}")
+        for county, reference in test_db.items():
+            for catalogue_reference, farm in reference.items(): 
+                assert isinstance(farm['Farm'], Farm)
+                # print(f"\t{test_db[county][farm].field_info_date} -> {test_db[county][farm].primary_record_date}")
 
