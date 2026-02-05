ef2a2a098bbeb557afff414be18449459612edbf
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-05 19:48:54 +0000

    test/refactor: use dict values instead of items as more succinct (?)

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -14,8 +14,9 @@ def test_nfs_checks_and_mergers():
         process_file(csv_file, test_mode=True)
 
     with shelve.open(PATH.TEST_DB, 'r') as test_db:
-        for county, reference in test_db.items():
-            for catalogue_reference, farm in reference.items(): 
+        for reference in test_db.values():
+            for farm in reference.values(): 
                 assert isinstance(farm['Farm'], Farm)
-                # print(f"\t{test_db[county][farm].field_info_date} -> {test_db[county][farm].primary_record_date}")
+                assert farm['source']
+
 
