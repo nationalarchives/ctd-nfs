3501ad47d69fb072d3d2ae0483373518933d8842
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-05 12:17:47 +0000

    test: recreate test db at start of test to make it easier to inspect of debug db if test fails

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -6,6 +6,9 @@ from src.file_processor import process_file
 from src.farm_builder import Farm
 
 def test_nfs_checks_and_mergers():
+    for file in Path(PATH.DB / "TEST").glob("*"):
+        os.remove(file)
+
     for csv_file in PATH.TEST_INPUT.glob("*.csv"):
         process_file(csv_file, test_mode=True)
 
@@ -15,4 +18,3 @@ def test_nfs_checks_and_mergers():
                 assert isinstance(test_db[county][farm], Farm)
                 print(f"\t{test_db[county][farm].field_info_date} -> {test_db[county][farm].primary_record_date}")
 
-    os.remove(PATH.TEST_DB)
