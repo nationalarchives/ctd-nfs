2f2bad056d93fe0144b10ae91690d991d9c3e6e1
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-08 18:20:08 +0000

    feat: add method to clean original values by stripping trailing spaces

--- src/discovery_data_processor.py
+++ src/discovery_data_processor.py
@@ -7,6 +7,21 @@ from src._tools.xlreader import read_file
 data_file = PATH.TEST_INPUT / "Rutland - Interim to Final.xlsx"
 
 
+def clean_excel_data(raw_csv_data: list[dict]) -> list[dict]:
+    discovery_data = []
+    for row in raw_csv_data:
+        cleaned_data_row = {}
+        for key, value in row.items():
+            if not value or type(value) is not str:
+                cleaned_data_row[key] = value
+                continue
+            value = value.strip()
+            cleaned_data_row[key] = value
+        discovery_data.append(cleaned_data_row)
+    
+    return discovery_data
+
+
 def load_excel_data() -> list[dict]:
     excel_data = read_file(data_file)
     column_names = excel_data['Sheet'][0]
@@ -19,4 +34,5 @@ def load_excel_data() -> list[dict]:
 
 if __name__ == "__main__":
     excel_data = load_excel_data()
+    cleaned_data = clean_excel_data(excel_data)
 
