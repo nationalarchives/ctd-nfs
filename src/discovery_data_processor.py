3911503c6d5387179fca35fc983346910d56108c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-08 18:05:04 +0000

    feat: add module to create Discovery data, with path to input file

---
+++ src/discovery_data_processor.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+import os
+
+from src._config.constants import PATH
+
+
+data_file = PATH.TEST_INPUT / "Rutland - Interrim to Final.xlsx"
+records_folder = PATH.TEST_OUTPUT / "Records"
+os.makedirs(records_folder, exist_ok=True)
+replica_folder = PATH.TEST_OUTPUT / "Replicas"
+os.makedirs(replica_folder, exist_ok=True)
+
