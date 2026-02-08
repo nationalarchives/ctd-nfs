b05417c798892fd0a031be6b02912106711d5113
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-08 16:48:59 +0000

    feat: add value for constant attributes

--- src/record_builder.py
+++ src/record_builder.py
@@ -23,17 +23,18 @@ class Record:
     parentId: str
     iaid: str
     replicaId: str
-    catalogueLevel: int
-    coveringFromDate: int
-    coveringToDate: int
-    chargeType: int
-    coveringDates: str
-    closureStatus: str
-    digitised: bool
+    """ constants """
+    catalogueLevel: int =  8
+    coveringFromDate: int = 19410101
+    coveringToDate: int = 19431231
+    chargeType: int =  1
+    coveringDates: str =  "1941-1943"
+    closureStatus: str =  "Open Document, Open Description"
+    digitised: bool =  True
     heldBy: list = field(default_factory=held_by)
-    legalStatus: str
-    referencePart: str
+    legalStatus: str = "Public Record(s)"
+    referencePart: str = "0"
     scopeContent: dict = field(default_factory=description)
-    source: str
-    title: str
+    source: str =  "FS"
+    title: str = "title"
 
