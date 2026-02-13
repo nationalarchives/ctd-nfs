2edd39c20da8bc293fbc3f371456dfac3fe35ec0
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-13 13:40:47 +0000

    refactor: update tests to use new fixture names

--- tests/unit/test_reference_values.py
+++ tests/unit/test_reference_values.py
@@ -2,37 +2,37 @@ from src.row_data_validator import has_valid_reference_values
 from tests.unit.conftest import setup
 
 
-def test_reference_values_bad_form(farm_reference_values_bad_form):
+def test_reference_values_bad_form(reference_values_bad_form):
 	pattern_matches, row_prefix = setup(
-		farm_reference_values_bad_form['data'],
-		farm_reference_values_bad_form['row_num']
+		reference_values_bad_form['data'],
+		reference_values_bad_form['row_num']
 		)
 
-	assert not has_valid_reference_values(farm_reference_values_bad_form['data'], pattern_matches, row_prefix)
+	assert not has_valid_reference_values(reference_values_bad_form['data'], pattern_matches, row_prefix)
 
 
-def test_reference_values_filename1_bad_pattern(farm_reference_values_filename1_bad_pattern):
+def test_reference_values_filename1_bad_pattern(reference_values_filename1_bad_pattern):
 	pattern_matches, row_prefix = setup(
-		farm_reference_values_filename1_bad_pattern['data'],
-		farm_reference_values_filename1_bad_pattern['row_num']
+		reference_values_filename1_bad_pattern['data'],
+		reference_values_filename1_bad_pattern['row_num']
 		)
 
-	assert not has_valid_reference_values(farm_reference_values_filename1_bad_pattern['data'], pattern_matches, row_prefix)
+	assert not has_valid_reference_values(reference_values_filename1_bad_pattern['data'], pattern_matches, row_prefix)
 
 
-def test_reference_values_filename2_bad_pattern(farm_reference_values_filename2_bad_pattern):
+def test_reference_values_filename2_bad_pattern(reference_values_filename2_bad_pattern):
 	pattern_matches, row_prefix = setup(
-		farm_reference_values_filename2_bad_pattern['data'],
-		farm_reference_values_filename2_bad_pattern['row_num']
+		reference_values_filename2_bad_pattern['data'],
+		reference_values_filename2_bad_pattern['row_num']
 		)
 
-	assert not has_valid_reference_values(farm_reference_values_filename2_bad_pattern['data'], pattern_matches, row_prefix)
+	assert not has_valid_reference_values(reference_values_filename2_bad_pattern['data'], pattern_matches, row_prefix)
 
 
-def test_reference_values_no_farm_data(farm_reference_values_no_farm_data):
+def test_reference_values_no_farm_data(reference_values_no_farm_data):
 	pattern_matches, row_prefix = setup(
-		farm_reference_values_no_farm_data['data'],
-		farm_reference_values_no_farm_data['row_num']
+		reference_values_no_farm_data['data'],
+		reference_values_no_farm_data['row_num']
 		)
 
-	assert not has_valid_reference_values(farm_reference_values_no_farm_data['data'], pattern_matches, row_prefix)
+	assert not has_valid_reference_values(reference_values_no_farm_data['data'], pattern_matches, row_prefix)
