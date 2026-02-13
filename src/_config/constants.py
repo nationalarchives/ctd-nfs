7eba276a544c593c3ddd33499f84dec50d31062f
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-02-13 13:28:51 +0000

    style: remove trailing spaces

--- src/_config/constants.py
+++ src/_config/constants.py
@@ -99,7 +99,7 @@ class RegexNamespace():
     __slots__ = ()
 
     FORM_PATTERN = re.compile(r"""^MAF32-(?P<piece>\d+)[-_](?P<parish_number>\d+)_+(?P<image_number>\d+)\.tif$""")
-    COVER_PATTERN = re.compile(r"""^MAF32-(?P<piece>\d+)[-_](?P<parish_number>\d+)\.tif$""")
+    COVER_PATTERN = re.compile(r"""^MAF32-(?P<piece>\d+)[-_](?P<parish_number>\d+)\.tif$""")   
     
     _date_delimiters: str = r"""[\/\.\-\s]+"""
     _month_names: list = "|".join(DataNamespace.MONTH_NAMES[1:])
