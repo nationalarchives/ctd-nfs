29477ff68a223318755ba317de605b8cc514f818
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-04 16:53:53 +0000

    feat: change to default value if no value provided, except for filename_2
    filename_2 remains at empty string to allow for regex testing

--- src/file_processor.py
+++ src/file_processor.py
@@ -46,7 +46,11 @@ def clean_csv_data(raw_csv_data: Iterator[dict]) -> Generator[dict, None, None]:
         for key, value in row.items():
             if key not in CSVEXCEL.CSV_HEADERS:
                 continue
-            if ";" in value:
+            if key == "filename_2" and value == "":
+                cleaned_data_row[key] = ""
+            elif value in ["", "*"]:
+                cleaned_data_row[key] = "[not specified]"
+            elif ";" in value:
                 cleaned_data_row[key] = split_list_values(value)
             else:
                 cleaned_data_row[key] = value.strip()
