b77e9b17b972c740dbaa7e94c51abb07bdb1c9bf
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-06 12:48:03 +0000

    style:

--- src/_tools/xlwriter.py
+++ src/_tools/xlwriter.py
@@ -19,7 +19,7 @@ class ExcelWriter:
         """
         for row_idx, record in enumerate(_records, start=1):
             self._sheet.append(record)
-            for col_idx in list(range(1,5)):
+            for col_idx in list(range(1, 5)):
                 column = ExcelWriter.column_letters[col_idx]            
                 self._sheet[f"{column}{row_idx + 1}"].alignment = Alignment(wrap_text=True, vertical='top')
 
