027e64b66e397080ce9ed4ffd6f3a0427966bff1
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-08 23:45:17 +0000

    feat: set alignment and position all output columns

--- src/_tools/xlwriter.py
+++ src/_tools/xlwriter.py
@@ -19,7 +19,7 @@ class ExcelWriter:
         """
         for row_idx, record in enumerate(_records, start=1):
             self._sheet.append(record)
-            for col_idx in list(range(1, 5)):
+            for col_idx in list(range(0, 21)):
                 column = ExcelWriter.column_letters[col_idx]            
                 self._sheet[f"{column}{row_idx + 1}"].alignment = Alignment(wrap_text=True, vertical='top')
 
@@ -30,7 +30,7 @@ class ExcelWriter:
 
     def _make_sheet(self, _index, sheet_name="", column_settings="", row_data=""):
         """
-
+    
         :param _index:
         :param sheet_name:
         :param column_settings:
