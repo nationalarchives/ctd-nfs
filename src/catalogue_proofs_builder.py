55484deb02a7648418ede0d9b7b85938b729c685
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-09 19:23:24 +0000

    feat: run against new county
    This is prior to a test being developed

--- src/catalogue_proofs_builder.py
+++ src/catalogue_proofs_builder.py
@@ -130,6 +130,9 @@ def _transform_farm_to_proof(farm: Farm) -> list:
 with shelve.open(PATH.TEST_DB, 'r') as farms_db:
     for county, references in farms_db.items():
         print(f"{county=}")
+        if county == 'RD Rutland':
+            continue
+
         proof_data = [
             _transform_farm_to_proof(references[catalogue_reference]['Farm'])
             for catalogue_reference in references.keys()
