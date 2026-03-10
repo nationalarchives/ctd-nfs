d02c37c569ad82140799854ea58bb5fc126a7ae9
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-10 16:28:15 +0000

    chore: add more log detail

--- src/file_processor.py
+++ src/file_processor.py
@@ -122,7 +122,9 @@ def process_file(csv_file: Path, test_mode: bool = False) -> None:
 
 
 if __name__ == "__main__":
+    logger.info(" ===== HARVESTING FARMS ===== ")
     for csv_file in PATH.INPUT.glob("*.csv"):
         process_file(csv_file)
 
+    logger.info(" ===== CREATING PROOF FILES ===== ")
     create_proof_files()
