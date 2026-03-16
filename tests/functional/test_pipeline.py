a44524d24d73f5ccfdc7ba607d73b54730c76bab
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-16 10:09:21 +0000

    feat: change to orchestrator structure as in harvester

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -3,7 +3,8 @@ import os
 from pathlib import Path
 
 from src._config.constants import PATH
-from src.harvester.file_processor import process_file
+from src.harvester.file_processor import process_csv_files
+from src.harvester.catalogue_proofs_builder import create_proof_files
 from src.harvester.farm_setup import Farm
 
 
@@ -11,8 +12,8 @@ def test_nfs_checks_and_mergers():
     for file in Path(PATH.DB / "TEST").glob("*"):
         os.remove(file)
 
-    for csv_file in PATH.TEST_INPUT.glob("*.csv"):
-        process_file(csv_file, test_mode=True)
+    process_csv_files(test_mode=True)
+    create_proof_files()
 
     with shelve.open(PATH.TEST_DB, 'r') as test_db:
         for reference in test_db.values():
