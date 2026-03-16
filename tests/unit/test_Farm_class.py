483694d34b139e55cd7629e11e5a71d2ad0e4141
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-16 17:12:18 +0000

    chore: update import to use new function name

--- tests/unit/test_Farm_class.py
+++ tests/unit/test_Farm_class.py
@@ -1,4 +1,4 @@
-from src.harvester.farm_setup import Farm, concatenate_instance, concatenate_forms, normalize_date
+from src.harvester.farm_setup import Farm, concatenate_instance, concatenate_forms, _normalize_date
 
 def test_farm_dataclass_instantiation(capsys, farms):
 	for farm_fixture in farms:
@@ -46,6 +46,6 @@ def test_normalize_date(valid_dates, normalized_dates):
 	for index, test_date in enumerate(valid_dates):
 		expected_result = normalized_dates[index]
 
-		assert expected_result == normalize_date(test_date)
+		assert expected_result == _normalize_date(test_date)
 
 
