91c05c20f8a3700a861823a2f7caf6ab96962ffc
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-16 10:08:15 +0000

    feat: add orchestrator function to create entry point for future installer

---
+++ harvester.py
@@ -0,0 +1,19 @@
+from src.harvester.file_processor import process_csv_files
+from src.harvester.catalogue_proofs_builder import create_proof_files
+from src._tools.logging_setup import create_logger
+
+
+logger = create_logger("src._config", "logging.yaml")
+
+
+def main():
+    logger.info(" ===== HARVESTING FARMS ===== ")
+    process_csv_files()
+
+    logger.info(" ===== CREATING PROOF FILES ===== ")
+    create_proof_files()
+
+
+if __name__ == "__main__":
+    main()
+
