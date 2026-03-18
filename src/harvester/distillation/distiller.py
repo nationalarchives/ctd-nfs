f59a1f01b95921d0023b2b1c1c4519b0ec5f1486
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-18 15:00:09 +0000

    feat: enable functions to handle titles

--- src/harvester/distillation/distiller.py
+++ src/harvester/distillation/distiller.py
@@ -1,15 +1,17 @@
 import re
 
 
-def distill_single_name_and_address(name: str, address: str) -> str:
-    if (name == "[not specified]" == address):
+def distill_single_name_and_address(title: str, name: str, address: str) -> str:
+    full_name = distill_single_title_and_name(title, name)
+
+    if (full_name == "[not specified]" == address):
         return "[not specified]"
 
     if address == "[not specified]":
-        return f"{name}"
+        return f"{full_name}"
 
-    if (name != "[not specified]" != address) or (name == "[not specified]"):
-        return f"{name}, {address}"
+    if (full_name != "[not specified]" != address) or (full_name == "[not specified]"):
+        return f"{full_name}, {address}"
 
 
 def distill_single_title_and_name(title: str, name: str) -> str:
@@ -25,13 +27,14 @@ def distill_single_title_and_name(title: str, name: str) -> str:
         return f"{name}"
 
 
-def distill_multiple_details(names: str, address: str) -> str:
+def distill_multiple_details(titles: str, names: str, address: str) -> str:
+    separated_titles = re.split("; *", titles)
     separated_names = re.split("; *", names)
     separated_addresses = re.split("; *", address)
 
     details = [
-        distill_single_name_and_address(name, address)
-        for name, address in zip(separated_names, separated_addresses)
+        distill_single_name_and_address(title, name, address)
+        for title, name, address in zip(separated_titles, separated_names, separated_addresses)
     ]
 
     return "; ".join(details)
