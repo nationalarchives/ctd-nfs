67ee104bfc61cca5b00d01ed257c55f07341162a
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-18 13:05:37 +0000

    feat: add new module for names and address distillaton and merging
    This is developed using TDD principles

---
+++ src/harvester/distillation/distiller.py
@@ -0,0 +1,24 @@
+import re
+
+def distill_single_name_and_address(name: str, address: str) -> str:
+    if (name == "[not specified]" == address):
+        return "[not specified]"
+
+    if address == "[not specified]":
+        return f"{name}"
+
+    if (name != "[not specified]" != address) or (name == "[not specified]"):
+        return f"{name}, {address}"
+
+
+def distill_multiple_details(names: str, address: str) -> str:
+    separated_names = re.split("; *", names)
+    separated_addresses = re.split("; *", address)
+
+    details = [
+        distill_single_name_and_address(name, address)
+        for name, address in zip(separated_names, separated_addresses)
+    ]
+
+    return "; ".join(details)
+
