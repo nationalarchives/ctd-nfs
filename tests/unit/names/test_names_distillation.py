621237e85ad8297d307ae7fb8ced3c41e2f6e448
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-18 14:28:26 +0000

    test: modify tests for single person to include title
    BREAKS TESTS FOR MULTIPLE DETAILS

--- tests/unit/names/test_names_distillation.py
+++ tests/unit/names/test_names_distillation.py
@@ -4,35 +4,58 @@ from src.harvester.distillation.distiller import \
     distill_single_title_and_name
 
 
-def test_name_with_address_both_specified():
+def test_single_person_with_full_details():
+    title = "Mr"
     name = "Sherlock Holmes"
     address = "221B Baker Street"
 
-    full_address = distill_single_name_and_address(name, address)
+    full_address = distill_single_name_and_address(title, name, address)
     
-    assert full_address == "Sherlock Holmes, 221B Baker Street"
+    assert full_address == "Mr Sherlock Holmes, 221B Baker Street"
 
 
-def test_only_name_specified():
+def test_single_person_with_full_details_and_appended_title():
+    title = "KC"
+    name = "Sherlock Holmes"
+    address = "221B Baker Street"
+
+    full_address = distill_single_name_and_address(title, name, address)
+    
+    assert full_address == "Sherlock Holmes, KC, 221B Baker Street"
+
+
+def test_single_person_with_only_name_specified():
+    title = "[not specified]"
     name = "Sherlock Holmes"
     address = "[not specified]"
-    full_address = distill_single_name_and_address(name, address)
+    full_address = distill_single_name_and_address(title, name, address)
     
     assert full_address == "Sherlock Holmes"
 
 
-def test_only_address_specified():
+def test_single_person_with_only_address_specified():
+    title = "[not specified]"
     name = "[not specified]"
     address = "221B Baker Street"
-    full_address = distill_single_name_and_address(name, address)
+    full_address = distill_single_name_and_address(title, name, address)
     
     assert full_address == "[not specified], 221B Baker Street"
 
 
-def test_both_not_specified():
+def test_single_person_with_only_title_specified():
+    title = "Mr"
     name = "[not specified]"
     address = "[not specified]"
-    full_address = distill_single_name_and_address(name, address)
+    full_address = distill_single_name_and_address(title, name, address)
+    
+    assert full_address == "[not specified]"
+
+
+def test_single_person_with_no_details():
+    title = "[not specified]"
+    name = "[not specified]"
+    address = "[not specified]"
+    full_address = distill_single_name_and_address(title, name, address)
     
     assert full_address == "[not specified]"
 
