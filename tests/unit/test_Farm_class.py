b0fb3f83dac86654e825aa0357be121cba2e5802
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-03-26 09:27:30 +0000

    docs: add explanation of usage of capsys

--- tests/unit/test_Farm_class.py
+++ tests/unit/test_Farm_class.py
@@ -1,6 +1,15 @@
 from src.harvester.farm_setup import Farm, concatenate_instance, concatenate_forms, _normalize_date
 
 def test_farm_dataclass_instantiation(capsys, farms):
+	"""_summary_
+	Args:
+		capsys (_type_): _description_
+		farms (_type_): _description_
+
+	capsys disabled from pytest docs https://docs.pytest.org/en/stable/how-to/capture-stdout-stderr.html#
+	To temporarily disable capture within a test, 
+	the capture fixtures have a disabled() method that can be used as a context manager, disabling capture inside the with block
+	"""	
 	for farm_fixture in farms:
 		new_farm = Farm(**farm_fixture)
 		with capsys.disabled():
