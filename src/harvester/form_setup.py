e48113ed1778731635da80ccd6172d1847e7260d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-02 17:51:24 +0100

    feat: create new Form dataclass which will be used to hold row data when loading from csv

---
+++ src/harvester/form_setup.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+
+type ListOrStr = list[str] | str
+
+
+@dataclass
+class Form:
+    """
+    all values except catalogue_reference will be instantiated from the raw csv data and then validated in a later step
+    all fields after primary_farm_number are lists to accomodate variation in names and addresses when original forms were filled out 
+    e.g., "Mr D. Smith", "D. Smith", "Dennis Smith Esq" entered as names for same person
+    These fields will be merged later to create a single name/address so are declared as either lists of strings or single strings
+    """
+    filename_1: str # TODO: create Filename ValueObject to include validation currently in row_data_validator.py
+    filename_2: str
+    document_type: str
+    county: str
+    parish: str
+    primary_farm_number: str
+    additional_farms: list[str]
+    farm_name: list[str]
+    addressee_title: ListOrStr
+    addressee_individual_name: ListOrStr
+    addressee_group_names: ListOrStr
+    address: ListOrStr
+    owner_title: ListOrStr
+    owner_individual_name: ListOrStr
+    owner_group_names: ListOrStr
+    owner_address: ListOrStr
+    farmer_title: ListOrStr
+    farmer_individual_name: ListOrStr
+    farmer_group_names: ListOrStr
+    farmer_address: ListOrStr    
+    acreage: ListOrStr
+    OS_map_sheet: ListOrStr
+    field_info_date: ListOrStr
+    primary_record_date: ListOrStr
+
