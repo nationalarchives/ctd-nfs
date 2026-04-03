6827bb03b485535fd74c77270663afa0378635dc
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-03 09:42:34 +0100

    feat: add functions for the regex match because cannot create attributes in post_init to a frozen dataclass

--- src/harvester/form_setup.py
+++ src/harvester/form_setup.py
@@ -15,15 +15,20 @@ class ValueObject:
     """A base class for all value objects"""
 
 
+def page_pattern_match(filename: str) -> re.Match:
+    return REGEX.FORM_PATTERN.match(filename)
+
+
+def cover_pattern_match(filename: str) -> re.Match:
+    return REGEX.COVER_PATTERN.match(filename)
+
+
 @dataclass(frozen=True)
 class Filename(ValueObject):
     name: str
 
-    def __post_init(self):
-        self.page_pattern_match: re.Match = REGEX.FORM_PATTERN.match(self.name)
-        self.cover_pattern_match: re.Match = REGEX.COVER_PATTERN.match(self.name)
-
-        if not (self.page_pattern_match or self.cover_pattern_match):
+    def __post_init__(self):
+        if not (page_pattern_match(self.name) and cover_pattern_match(self.name)):
             raise BusinessRuleValidationException(f"{self.name} does not match expected pattern for form images or cover.")
         
     def is_cover(self) -> bool:
