4a62a432dbf7c620aa0165adf6b123bbfe7b362b
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-07 09:01:35 +0100

    test: rename test module to match new function name

--- tests/unit/test_transcription_model.py
+++ tests/unit/test_transcription_model.py
@@ -1,30 +1,30 @@
 import pytest
 
 from src._tools.helpers import DomainRuleValidationException
-from src._dataclasses.transcription_model import Filename, FormType
+from src._dataclasses.transcription_model import File, FormType
 
 
 def test_valid_filename():
     fixture = "MAF32-194-1_59.tif"
-    test = Filename(fixture)
+    test = File(fixture)
 
-    assert isinstance(test, Filename)
+    assert isinstance(test, File)
     assert test.is_cover is False
 
 
 def test_cover_image_1():
     fixture = "MAF32-51-285_0001.tif"
-    test = Filename(fixture)
+    test = File(fixture)
 
-    assert isinstance(test, Filename)
+    assert isinstance(test, File)
     assert test.is_cover
 
 
 def test_cover_image_2():
     fixture = "MAF32-51-285.tif"
-    test = Filename(fixture)
+    test = File(fixture)
 
-    assert isinstance(test, Filename)
+    assert isinstance(test, File)
     assert test.is_cover
     assert test.image_number is None
 
@@ -33,14 +33,14 @@ def test_bad_image_name():
     fixture = "MAF3251286.tif"
 
     with pytest.raises(DomainRuleValidationException):
-        Filename(fixture)
+        File(fixture)
 
 
 def test_properties():
     fixture = "MAF32-194-1_59.tif"
-    test = Filename(fixture)
+    test = File(fixture)
 
-    assert isinstance(test, Filename)
+    assert isinstance(test, File)
     assert test.image_number == 59
     assert test.piece == "194"
     assert test.parish_number == "1"
