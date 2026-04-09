df17a868c44f2f2ed7ed87b7ebec734b22de169f
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-09 13:53:13 +0100

    fix: add post_init attributes to list of attributes to be excluded from search

--- src/_dataclasses/transcription_model.py
+++ src/_dataclasses/transcription_model.py
@@ -92,7 +92,11 @@ class Transcription:
         return any([
             getattr(self, field_name) != "[not specified]"
             for field_name in self.__dict__
-            if field_name not in ['filename_1', 'filename_2','document_type', 'county', 'parish']
+            if field_name not in [
+                'filename_1', 'filename_2',
+                'document_type', 'county', 'parish', 
+                'file1', 'file2', 'form_type'
+                ]
         ])
     
     # @property
