16568602e4be6c9dfc3f41fac7a70fb6e1f31a78
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-12 09:48:10 +0100

    test: copy fixture from test_filename_validation to mimic same use

--- tests/unit/transcription/test_cover_issues.py
+++ tests/unit/transcription/test_cover_issues.py
@@ -1,5 +1,28 @@
-from src.harvester.row_data_validator import check_for_cover_with_farm_details, report_cover_image_inconsistencies
-from tests.unit.conftest import setup
+from src._dataclasses.transcription_model import Transcription
+from src._dataclasses.transcription_checker import TranscriptionChecker
+
+
+fixture = {
+	'primary_farm_number': "18",
+	'additional_farms': "Outhouse, No 1",
+	'farm_name': "The Grove Farm",
+	'addressee_title': "[not specified]",
+	'addressee_individual_name': "[not specified]",
+	'addressee_group_names': "[not specified]",
+	'address': "[not specified]",
+	'owner_title': "Rev",
+	'owner_individual_name': "H R Fleming",
+	'owner_group_names': "[not specified]",
+	'owner_address': "Rayrigg Hall, Windermere, Westmorland",
+	'farmer_title': "[not specified]",
+	'farmer_individual_name': "R Nicholson",
+	'farmer_group_names': "[not specified]",
+	'farmer_address': "The Grove Farm, Ambleside",
+	'acreage': "162.5/886.5/1049",
+	'OS_map_sheet': "26 NE",
+	'field_info_date': "06-Feb-42",
+	'primary_record_date': "12-Feb-43",
+}
 
 
 def test_cover_image_inconsistencies_bad_cover_pattern(cover_image_inconsistencies_bad_cover_pattern):
