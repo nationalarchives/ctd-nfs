3139bbaebccf2fc93254a34cba24bd890f0cfc21
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-12 10:57:55 +0100

    test: replace module fixture with conftest fixture

--- tests/unit/transcription/test_cover_issues.py
+++ tests/unit/transcription/test_cover_issues.py
@@ -2,38 +2,15 @@ from src._dataclasses.transcription_model import Transcription
 from src._dataclasses.transcription_checker import TranscriptionChecker
 
 
-fixture = {
-	'primary_farm_number': "18",
-	'additional_farms': "Outhouse, No 1",
-	'farm_name': "The Grove Farm",
-	'addressee_title': "[not specified]",
-	'addressee_individual_name': "[not specified]",
-	'addressee_group_names': "[not specified]",
-	'address': "[not specified]",
-	'owner_title': "Rev",
-	'owner_individual_name': "H R Fleming",
-	'owner_group_names': "[not specified]",
-	'owner_address': "Rayrigg Hall, Windermere, Westmorland",
-	'farmer_title': "[not specified]",
-	'farmer_individual_name': "R Nicholson",
-	'farmer_group_names': "[not specified]",
-	'farmer_address': "The Grove Farm, Ambleside",
-	'acreage': "162.5/886.5/1049",
-	'OS_map_sheet': "26 NE",
-	'field_info_date': "06-Feb-42",
-	'primary_record_date': "12-Feb-43",
-}
-
-
-def test_cover_image_inconsistencies_bad_cover_pattern():
-	fixture.update({
+def test_cover_image_inconsistencies_bad_cover_pattern(farm):
+	farm.update({
 		'filename_1': "MAF32-194-1_59.tif",
 		'filename_2': "",
 		'document_type': "Cover",
 		'county': "WD Westmorland",
 		'parish': "1 Ambleside",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -43,15 +20,15 @@ def test_cover_image_inconsistencies_bad_cover_pattern():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_cover_image_inconsistencies_cover_with_two_images_1():
-	fixture.update({
+def test_cover_image_inconsistencies_cover_with_two_images_1(farm):
+	farm.update({
 		'filename_1': "MAF32-193-203_97.tif",
 		'filename_2': "MAF32-193-203_98.tif",
 		'document_type': "Cover",
 		'county': "CU Cumberland",
 		'parish': "203 Winscales",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -61,15 +38,15 @@ def test_cover_image_inconsistencies_cover_with_two_images_1():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_cover_image_inconsistencies_cover_with_two_images_2():
-	fixture.update({
+def test_cover_image_inconsistencies_cover_with_two_images_2(farm):
+	farm.update({
 		'filename_1': "MAF32-51-285_0001.tif",
 		'filename_2': "MAF32-51-285_0002.tif",
 		'document_type': "Cover",
 		'county': "WL Wiltshire",
 		'parish': "285 Zeals",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -79,15 +56,15 @@ def test_cover_image_inconsistencies_cover_with_two_images_2():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_cover_image_inconsistencies_form_supplied_but_cover_image():
-	fixture.update({
+def test_cover_image_inconsistencies_form_supplied_but_cover_image(farm):
+	farm.update({
 		'filename_1': "MAF32-51-285_0001.tif",
 		'filename_2': "",
 		'document_type': "SF",
 		'county': "WL Wiltshire",
 		'parish': "285 Zeals",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -97,15 +74,15 @@ def test_cover_image_inconsistencies_form_supplied_but_cover_image():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_cover_image_inconsistencies_form_supplied_but_cover_pattern():
-	fixture.update({
+def test_cover_image_inconsistencies_form_supplied_but_cover_pattern(farm):
+	farm.update({
 		'filename_1': "MAF32-51-285.tif",
 		'filename_2': "",
 		'document_type': "SF",
 		'county': "WL Wiltshire",
 		'parish': "285 Zeals",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
--- tests/unit/transcription/test_filename_validation.py
+++ tests/unit/transcription/test_filename_validation.py
@@ -2,38 +2,15 @@ from src._dataclasses.transcription_model import Transcription
 from src._dataclasses.transcription_checker import TranscriptionChecker
 
 
-fixture = {
-	'primary_farm_number': "18",
-	'additional_farms': "Outhouse, No 1",
-	'farm_name': "The Grove Farm",
-	'addressee_title': "[not specified]",
-	'addressee_individual_name': "[not specified]",
-	'addressee_group_names': "[not specified]",
-	'address': "[not specified]",
-	'owner_title': "Rev",
-	'owner_individual_name': "H R Fleming",
-	'owner_group_names': "[not specified]",
-	'owner_address': "Rayrigg Hall, Windermere, Westmorland",
-	'farmer_title': "[not specified]",
-	'farmer_individual_name': "R Nicholson",
-	'farmer_group_names': "[not specified]",
-	'farmer_address': "The Grove Farm, Ambleside",
-	'acreage': "162.5/886.5/1049",
-	'OS_map_sheet': "26 NE",
-	'field_info_date': "06-Feb-42",
-	'primary_record_date': "12-Feb-43",
-}
-
-
-def test_values_between_filenames_different_pieces():
-	fixture.update({
+def test_values_between_filenames_different_pieces(farm):
+	farm.update({
 		'filename_1': "MAF32-194-1_59.tif",
 		'filename_2': "MAF32-195-1_60.tif",
 		'document_type': "C51/SSY",
 		'county': "WD Westmorland",
 		'parish': "1 Ambleside",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -43,15 +20,15 @@ def test_values_between_filenames_different_pieces():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_values_between_filenames_different_parish_numbers():
-	fixture.update({
+def test_values_between_filenames_different_parish_numbers(farm):
+	farm.update({
 		'filename_1': "MAF32-5-98_28.tif",
 		'filename_2': "MAF32-5-96_29.tif",
 		'document_type': "C51/SSY",
 		'county': "WD Westmorland",
 		'parish': "1 Ambleside",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -61,15 +38,15 @@ def test_values_between_filenames_different_parish_numbers():
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_values_between_filenames_image_number_error(values_between_filenames_image_number_error):
-	fixture.update({
+def test_values_between_filenames_image_number_error(farm):
+	farm.update({
 		'filename_2': "MAF32-167-28_388.tif",
 		'filename_1': "MAF32-167-28_386.tif",
 		'document_type': "C 47/SSY",
 		'county': "CU Cumberland",
 		'parish': "28 Aikton",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
@@ -79,15 +56,15 @@ def test_values_between_filenames_image_number_error(values_between_filenames_im
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
 
-def test_values_between_filenames_parish_number_mismatch(values_between_filenames_parish_number_mismatch):
-	fixture.update({
+def test_values_between_filenames_parish_number_mismatch(farm):
+	farm.update({
 		'filename_1': "MAF32-5-96_28.tif",
 		'filename_2': "MAF32-5-96_29.tif",
 		'document_type': "B496/EI",
 		'county': "HF Herefordshire",
 		'parish': "98 Clehonger",
 	})
-	transcription = Transcription(**fixture)
+	transcription = Transcription(**farm)
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
