6e05f318301247944ddb0122a831bf7e9454197d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-12 10:20:44 +0100

    fix: correct method to _report_cover_image_inconsistencies

--- tests/unit/transcription/test_cover_issues.py
+++ tests/unit/transcription/test_cover_issues.py
@@ -37,7 +37,7 @@ def test_cover_image_inconsistencies_bad_cover_pattern():
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
-	checker._check_values_between_filenames()
+	checker._report_cover_image_inconsistencies()
 
 	expected_message = f"{checker.row_prefix}Form type is 'Cover' but MAF32-194-1_59.tif does not match expected cover pattern or have image number 0001."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
@@ -55,7 +55,7 @@ def test_cover_image_inconsistencies_cover_with_two_images_1():
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
-	checker._check_values_between_filenames()
+	checker._report_cover_image_inconsistencies()
 
 	expected_message = f"{checker.row_prefix}Form type is 'Cover' but two form images were provided: MAF32-193-203_97.tif and MAF32-193-203_98.tif."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
@@ -73,7 +73,7 @@ def test_cover_image_inconsistencies_cover_with_two_images_2():
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
-	checker._check_values_between_filenames()
+	checker._report_cover_image_inconsistencies()
 
 	expected_message = f"{checker.row_prefix}Form type is 'Cover', and MAF32-51-285_0001.tif matches expected pattern for cover image but additional image MAF32-51-285_0002.tif was also provided."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
@@ -91,7 +91,7 @@ def test_cover_image_inconsistencies_form_supplied_but_cover_image():
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
-	checker._check_values_between_filenames()
+	checker._report_cover_image_inconsistencies()
 
 	expected_message = f"{checker.row_prefix}MAF32-51-285_0001.tif matches expected cover pattern or has image number 0001 but form type is 'SF'."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
@@ -109,7 +109,7 @@ def test_cover_image_inconsistencies_form_supplied_but_cover_pattern():
 	row_number = "10"
 
 	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
-	checker._check_values_between_filenames()
+	checker._report_cover_image_inconsistencies()
 
 	expected_message = f"{checker.row_prefix}MAF32-51-285.tif matches expected cover pattern or has image number 0001 but form type is 'SF'."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
