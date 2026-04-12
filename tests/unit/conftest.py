e8c1febaad9cea8d33d10dac49bed457ec213688
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-12 10:50:27 +0100

    test: extract fixture to transcription/conftest

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -7,31 +7,6 @@ from src._dataclasses.transcription_model import Filename, FormType
 # from src._dataclasses.transcription_checker import initialise_warnings_mapping
 
 
-@pytest.fixture()
-def transcription_checker_base():
-	return {
-		'primary_farm_number': "18",
-		'additional_farms': "Outhouse, No 1",
-		'farm_name': "The Grove Farm",
-		'addressee_title': "[not specified]",
-		'addressee_individual_name': "[not specified]",
-		'addressee_group_names': "[not specified]",
-		'address': "[not specified]",
-		'owner_title': "Rev",
-		'owner_individual_name': "H R Fleming",
-		'owner_group_names': "[not specified]",
-		'owner_address': "Rayrigg Hall, Windermere, Westmorland",
-		'farmer_title': "[not specified]",
-		'farmer_individual_name': "R Nicholson",
-		'farmer_group_names': "[not specified]",
-		'farmer_address': "The Grove Farm, Ambleside",
-		'acreage': "162.5/886.5/1049",
-		'OS_map_sheet': "26 NE",
-		'field_info_date': "06-Feb-42",
-		'primary_record_date': "12-Feb-43",
-	}
-
-
 def setup(test_data, row_num) -> tuple:
 	pattern_matches: dict[re.Match] = {
 		'filename_1': REGEX.FORM_PATTERN.match(test_data['filename_1']),
---
+++ tests/unit/transcription/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.fixture()
+def transcription_checker_base():
+	return {
+		'primary_farm_number': "18",
+		'additional_farms': "Outhouse, No 1",
+		'farm_name': "The Grove Farm",
+		'addressee_title': "[not specified]",
+		'addressee_individual_name': "[not specified]",
+		'addressee_group_names': "[not specified]",
+		'address': "[not specified]",
+		'owner_title': "Rev",
+		'owner_individual_name': "H R Fleming",
+		'owner_group_names': "[not specified]",
+		'owner_address': "Rayrigg Hall, Windermere, Westmorland",
+		'farmer_title': "[not specified]",
+		'farmer_individual_name': "R Nicholson",
+		'farmer_group_names': "[not specified]",
+		'farmer_address': "The Grove Farm, Ambleside",
+		'acreage': "162.5/886.5/1049",
+		'OS_map_sheet': "26 NE",
+		'field_info_date': "06-Feb-42",
+		'primary_record_date': "12-Feb-43",
+	}
