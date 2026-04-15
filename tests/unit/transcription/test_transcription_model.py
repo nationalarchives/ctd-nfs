8c1dfdc440dbe3f6be2fe15a96de959657085f92
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-15 21:10:07 +0100

    test: add test_repr

--- tests/unit/transcription/test_transcription_model.py
+++ tests/unit/transcription/test_transcription_model.py
@@ -134,3 +134,36 @@ def test_page_pattern_with_no_transcription_data():
     test = Transcription(**fixture)
     assert test.no_data
 
+
+def test_repr():
+    fixture = {
+        'filename_1': "MAF32-194-1_59.tif",
+        'filename_2': "MAF32-194-1_60.tif",
+        'document_type': "B496/EI",
+        'county': "WD Westmorland",
+        'parish': "1 Ambleside",
+        'primary_farm_number': "18",
+        'additional_farms': "Outhouse, No 1",
+        'farm_name': "The Grove Farm",
+        'addressee_title': "[not specified]",
+        'addressee_individual_name': "[not specified]",
+        'addressee_group_names': "[not specified]",
+        'address': "[not specified]",
+        'owner_title': "Rev",
+        'owner_individual_name': "H R Fleming",
+        'owner_group_names': "[not specified]",
+        'owner_address': "Rayrigg Hall, Windermere, Westmorland",
+        'farmer_title': "[not specified]",
+        'farmer_individual_name': "R Nicholson",
+        'farmer_group_names': "[not specified]",
+        'farmer_address': "The Grove Farm, Ambleside",
+        'acreage': "162.5/886.5/1049",
+        'OS_map_sheet': "26 NE",
+        'field_info_date': "06-Feb-42",
+        'primary_record_date': "12-Feb-43",
+    }
+
+    test = Transcription(**fixture)
+    assert eval(repr(test))['document_type'] == "B496/EI"
+
+	
