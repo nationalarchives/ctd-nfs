b530bd5e96cc3424503b1daeb0b9f92fce964d8c
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-15 20:59:06 +0100

    test: add consecutive images test

--- tests/unit/transcription/test_filename_validation.py
+++ tests/unit/transcription/test_filename_validation.py
@@ -73,3 +73,21 @@ def test_values_between_filenames_parish_number_mismatch(farm):
 	expected_message = f"{checker.row_prefix}MAF32-5-96_28.tif and MAF32-5-96_29.tif have a different parish number from parish name '98 Clehonger'."
 	assert expected_message == checker.warnings['Filename Warnings'][0]
 
+
+def test_not_consecutive_images(farm):
+	farm.update({
+		'filename_1': "MAF32-167-28_386.tif",
+		'filename_2': "MAF32-167-28_385.tif",
+		'document_type': "C 47/SSY",
+		'county': "CU Cumberland",
+		'parish': "28 Aikton",
+	})
+	transcription = Transcription(**farm)
+	row_number = "10"
+
+	checker = TranscriptionChecker(transcription=transcription, row_number=row_number)
+	checker._check_values_between_filenames()
+
+	expected_message = f"{checker.row_prefix}MAF32-167-28_386.tif and MAF32-167-28_385.tif are either not consecutive images or in the wrong order."
+	assert expected_message == checker.warnings['Filename Warnings'][0]
+
