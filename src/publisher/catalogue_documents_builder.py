b3c16ea660363f4a3c2254e59f589c6d9d3cd003
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-23 15:07:04 +0100

    refactor: move functions to catalogue_documents_publisher
    No need for separate module with only two short functions

--- src/publisher/catalogue_documents_builder.py
+++ src/publisher/catalogue_documents_builder.py
@@ -1,42 +1,11 @@
-import shelve
 import logging
 
-from src._tools.constants import PATH
-from src._dataclasses.farm_model import Farm, Details
-from src._dataclasses.discovery_model import Discovery
 
 
 logger = logging.getLogger(__name__)
 
 
-def _get_farm_instance(catalogue_reference: str) -> Farm:
-    # TODO: parse county from filename
-    county_name = "RD Rutland"
-    with shelve.open(PATH.FARMS_DB, "r") as farms_db:
-        return farms_db[county_name][catalogue_reference]['farm']
 
 
 
-def build_catalogue_documents(cleaned_data: list[dict], test_mode=False) -> list[dict]:
-    logger.info(" ===== BUILDING DISCOVERY RECORDS ===== ")
-
-    documents = []
-    for row in cleaned_data:
-        farm = _get_farm_instance(row['catalogue_reference'])
-        farm.farm_name = row['farm_name']
-        farm.landowner = Details(full_address=row['landowner'])
-        farm.farmer = Details(full_address=row['farmer'])
-        farm.addressee = Details(full_address=row['addressee'])
-        farm.acreage = row['acreage']
-        farm.OS_map_sheet = row['os_sheet_number']
-        farm.field_info_date = row['field_info_date']
-        farm.primary_record_date = row['primary_record_date']
-        farm.additional_farms = row['additional_farms']
-
-        discovery_document = Discovery(farm)
-        logger.info(f"Farm {farm.catalogue_reference} --> Built record {farm.iaid} with {len(discovery_document.files)} images")
-        
-        documents.append(discovery_document.to_dict())
-
-    return documents
 
--- src/publisher/catalogue_documents_publisher.py
+++ src/publisher/catalogue_documents_publisher.py
@@ -2,14 +2,18 @@
 """
 import json
 import pprint
+import shelve
 
 from src._tools.constants import PATH
 from src._tools.xlreader import read_file
 from src._tools.logging_setup import create_logger
-from src.publisher.catalogue_documents_builder import build_catalogue_documents
+from src._dataclasses.discovery_model import Discovery
+from src._dataclasses.farm_model import Details, Farm
 
 
 logger = create_logger("src._config", "logging.yaml")
+
+
 pretty = pprint.PrettyPrinter(indent=4)
 
 
@@ -47,6 +51,37 @@ def clean_excel_data(raw_csv_data: list[dict]) -> list[dict]:
         discovery_data.append(cleaned_data_row)
     
     return discovery_data
+
+
+def _get_farm_instance(catalogue_reference: str) -> Farm:
+    # TODO: parse county from filename
+    county_name = "RD Rutland"
+    with shelve.open(PATH.FARMS_DB, "r") as farms_db:
+        return farms_db[county_name][catalogue_reference]['farm']
+
+
+def build_catalogue_documents(cleaned_data: list[dict], test_mode=False) -> list[dict]:
+    logger.info(" ===== BUILDING DISCOVERY RECORDS ===== ")
+
+    documents = []
+    for row in cleaned_data:
+        farm = _get_farm_instance(row['catalogue_reference'])
+        farm.farm_name = row['farm_name']
+        farm.landowner = Details(full_address=row['landowner'])
+        farm.farmer = Details(full_address=row['farmer'])
+        farm.addressee = Details(full_address=row['addressee'])
+        farm.acreage = row['acreage']
+        farm.OS_map_sheet = row['os_sheet_number']
+        farm.field_info_date = row['field_info_date']
+        farm.primary_record_date = row['primary_record_date']
+        farm.additional_farms = row['additional_farms']
+
+        discovery_document = Discovery(farm)
+        logger.info(f"Farm {farm.catalogue_reference} --> Built record {farm.iaid} with {len(discovery_document.files)} images")
+
+        documents.append(discovery_document.to_dict())
+
+    return documents
 
 
 def write_catalogue_documents(documents: list[dict]) -> None:
