55bdd690b1d48aa7e9826677779d48d9cbc1ab73
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-04-23 10:10:42 +0100

    refactor: create new version of record_setup as discovery_model
    This will replace record_setup

---
+++ src/_dataclasses/discovery_model.py
@@ -0,0 +1,81 @@
+"""
+Dataclasses and factories used to create Discovery JSON records
+"""
+from dataclasses import dataclass, field, InitVar
+import requests
+from urllib import parse
+
+from src._tools.constants import DISCOVERY
+from src._tools.helpers import create_uuid_str
+
+
+def scope_and_content():
+    return {
+        'description': "",
+        }
+
+
+def _get_parent_id(catalague_reference: str) -> str:
+    ref = catalague_reference.rsplit("/", maxsplit=1)[0]
+    ref_url_safe = parse.quote(ref)
+
+    api_query = fr"{DISCOVERY.API_URI}/search/records?sps.searchQuery={ref_url_safe}"
+    result = requests.get(api_query)
+    parent_record = result.json()
+
+    return parent_record['records'][0]['id']
+
+
+@dataclass
+class Record:    
+    iaid: str # must be same as iaid from farm instance
+    citableReference: str # catalogue reference e.g. "MAF 32/348/56/12"
+    replicaId: str = field(default_factory=create_uuid_str)
+    scopeContent: dict = field(default_factory=scope_and_content)
+
+    def __post_init__(self):
+        self.parentId = _get_parent_id(self.citableReference)
+
+    def __repr__(self):       
+        return str({
+            'iaid': self.iaid,
+            'citableReference': self.citableReference,
+            'replicaId': self.replicaId,
+            'parentId': self.parentId,
+            'scopeContent': self.scopeContent,
+        } | DISCOVERY.RECORD_CONSTANTS)
+        
+
+@dataclass
+class Image:
+    originalName: str
+    id: InitVar[str] = ""
+    
+    def __post_init__(self, id):
+        self.name = f"66/MAF/32/{id}.jpg"
+
+
+@dataclass
+class Replica:
+    replicaId: str # must be same as replicaId in record instance
+    files: list[Image]
+
+    def __repr__(self):
+        images = [
+            {
+            'originalName': image.originalName,
+            'format': "jpg",
+            'name': image.name,
+            }
+            for image in self.files
+        ]
+        
+        return str({
+        'files': images,
+        'replicaId': self.replicaId,
+        'origination': "DigitalSurrogate",
+        'totalSize': None,
+        })
+    
+
+
