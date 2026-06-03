66854a5c8df0aab8a7d6d91f13f64fd013c19044
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-06-03 16:04:10 +0100

    chore: remove deprecated tests

--- tests/unit/addresses/test_address_merging.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from src.harvester.details_still import distill_details
-
-
-@pytest.mark.skip(reason="names and address distillation functions have been changed")
-def test_distill_address(addresses):
-    for fixture in addresses:
-        assert distill_details(fixture['original']) == fixture['final']
