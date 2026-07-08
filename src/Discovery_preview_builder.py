236b2e36dcc3bd6cb097b12954838d99f5eb3d28
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-07-08 23:09:32 +0100

    feat: add MAF32 to search path
    proof file name pattern will include MAF32 or MAF73

--- src/Discovery_preview_builder.py
+++ src/Discovery_preview_builder.py
@@ -111,7 +111,7 @@ def load_excel_data(data_file: Path) -> list[dict]:
            
             
 def process_proof_files(test_mode: bool=False) -> None:
-    xlsx_files = PATH.TEST_PUBLISH.glob("*.xlsx") if test_mode else PATH.PUBLISH.glob("*.xlsx")
+    xlsx_files = PATH.TEST_PUBLISH.glob("*MAF32*.xlsx") if test_mode else PATH.PUBLISH.glob("*MAF32*.xlsx")
 
     for proof_file in xlsx_files:
         county, _ = proof_file.name.split("_", maxsplit=1)
