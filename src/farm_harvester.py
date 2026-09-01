57c1265d3cbee90f92eccddf61704c5e37ea5e43
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-01 18:35:15 +0100

    feat: add test-mode argument

--- src/farm_harvester.py
+++ src/farm_harvester.py
@@ -344,7 +344,7 @@ def run_pipeline(test_mode: bool=False) -> None:
         write_farms_to_db(farms_store, county, test_mode)
 
         logger.info(" ===== CREATING PROOF FILE ===== ")
-        proof_file, preview_data = create_proof_file(farms_store, county, csv_file.stem)
+        proof_file, preview_data = create_proof_file(farms_store, county, csv_file.stem, test_mode)
 
         logger.info(" ===== CREATING HTML PREVIEW ===== ")
         create_html_preview(proof_file, excel_data=preview_data)
