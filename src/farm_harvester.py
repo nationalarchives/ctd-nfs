999cfb5fd11e8ea17df1addb1595a973ce902033
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-01 18:34:52 +0100

    feat: change search string so that  later  can be found
    This means we can test V1 &c before performing the production run

--- src/farm_harvester.py
+++ src/farm_harvester.py
@@ -326,7 +326,7 @@ def run_pipeline(test_mode: bool=False) -> None:
     Keyword Arguments:
         test_mode -- _description_ (default: {False})
     """    
-    input_files = PATH.INPUT.glob("TEST/*MAF32_HarvesterIN_V0*.csv") if test_mode else PATH.INPUT.glob("*MAF32_HarvesterIN_V*.csv")
+    input_files = PATH.INPUT.glob("TEST/*MAF32_HarvesterIN_*.csv") if test_mode else PATH.INPUT.glob("*MAF32_HarvesterIN_*.csv")
     logger.info(" ===== HARVESTING FARMS ===== ")
 
     for csv_file in input_files:
