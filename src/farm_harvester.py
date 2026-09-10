700bacb7cc36b75e9f20f513bf933a816a521016
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-10 13:39:26 +0100

    feat: add total lines in file stat to log info message

--- src/farm_harvester.py
+++ src/farm_harvester.py
@@ -313,7 +313,8 @@ def load_data_from_file(csv_file: Path) -> Generator[dict]:
     try:
         with open(csv_file, newline='') as file_obj:
             raw_csv_data = csv.DictReader(file_obj, skipinitialspace=True)
-            logger.info(f" ===== PROCESSING FILE: {csv_file.stem} ===== ")
+            total_lines = raw_csv_data.line_num
+            logger.info(f" ===== PROCESSING FILE: {csv_file.stem} with {total_lines} lines ===== ")
             yield from raw_csv_data
 
     except csv.Error as csv_error_message:
