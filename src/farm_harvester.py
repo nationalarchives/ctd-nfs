5f4ca52a88344291c447fe17f23d4c029d438216
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-10 16:03:15 +0100

    fix; remove stat for number of lines
    Does not work with csv.DictReader

--- src/farm_harvester.py
+++ src/farm_harvester.py
@@ -309,8 +309,7 @@ def load_data_from_file(csv_file: Path) -> Generator[dict]:
     try:
         with open(csv_file, newline='') as file_obj:
             raw_csv_data = csv.DictReader(file_obj, skipinitialspace=True)
-            total_lines = raw_csv_data.line_num
-            logger.info(f" ===== PROCESSING FILE: {csv_file.stem} with {total_lines} lines ===== ")
+            logger.info(f" ===== PROCESSING FILE: {csv_file.stem} ===== ")
             yield from raw_csv_data
 
     except csv.Error as csv_error_message:
