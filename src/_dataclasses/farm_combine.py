c5ba6c0a0a2a8aef3deb9a9c1d756e5ec5e45ff1
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-17 13:30:15 +0100

    feat: move function so that declared before called by post_init

--- src/_dataclasses/farm_combine.py
+++ src/_dataclasses/farm_combine.py
@@ -294,6 +294,25 @@ class HarvestedFarm:
 @dataclass
 class PublishedFarm:
     farm: HarvestedFarm
+
+    def _process_forms_for_proof(self) -> None:
+        forms_in_proof_format = []
+        files_in_proof_format = []
+        ids_in_proof_format = []
+
+        for form_type, image_sets in self.farm.forms.items():
+            if len(image_sets) == 1:
+                forms_in_proof_format.append(form_type)
+            else:
+                for index in range(len(image_sets)):
+                    forms_in_proof_format.append(f"{form_type} ({index + 1})")
+            for images in image_sets:
+                files_in_proof_format.append(", ".join([_img.name for _img in images]))
+                ids_in_proof_format.append(", ".join([_img.id for _img in images]))
+
+        self.forms = forms_in_proof_format
+        self.file_names = files_in_proof_format
+        self.file_ids = ids_in_proof_format
             
     @staticmethod
     def _join(values: list, newline=True) -> str:
@@ -318,25 +337,6 @@ class PublishedFarm:
         self.primary_record_date = self._join(self.farm.primary_record_date)
 
         self._process_forms_for_proof()
-
-    def _process_forms_for_proof(self) -> None:
-        forms_in_proof_format = []
-        files_in_proof_format = []
-        ids_in_proof_format = []
-
-        for form_type, image_sets in self.farm.forms.items():
-            if len(image_sets) == 1:
-                forms_in_proof_format.append(form_type)
-            else:
-                for index in range(len(image_sets)):
-                    forms_in_proof_format.append(f"{form_type} ({index + 1})")
-            for images in image_sets:
-                files_in_proof_format.append(", ".join([_img.name for _img in images]))
-                ids_in_proof_format.append(", ".join([_img.id for _img in images]))
-
-        self.forms = forms_in_proof_format
-        self.file_names = files_in_proof_format
-        self.file_ids = ids_in_proof_format
 
 
 @dataclass
