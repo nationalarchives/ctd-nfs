f830b919b3d13873726076babce9eebe8e3fcb6d
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-18 12:23:23 +0100

    refactor: change to use DB constant

--- tests/functional/test_pipeline.py
+++ tests/functional/test_pipeline.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pytest
 
 from src._dataclasses.farm_combine import HarvestedFarm
-from src._tools.constants import PATH
+from src._tools.constants import DB
 from src._tools.logging_setup import create_logger
 from src.farm_harvester import create_proof_file, run_pipeline
 
@@ -19,7 +19,7 @@ def test_file_processor():
 
     run_pipeline(test_mode=True)
 
-    with shelve.open(PATH.TEST_DB, 'r') as test_db:
+    with shelve.open(DB.TEST, 'r') as test_db:
         for reference in test_db.values():
             for farm in reference.values(): 
                 assert isinstance(farm['farm'], HarvestedFarm)
