4a18bc5019f65bbcd5f6d4c9c029014707bd1753
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-18 23:36:54 +0100

    feat: return list of farm instances

--- src/farm_publisher.py
+++ src/farm_publisher.py
@@ -65,7 +65,7 @@ def process_proof_files(test_mode: bool=False) -> None:
         version = REGEX.TRANSCRIPTIONS_VERSION.match(csv_file.stem)['version']
 
         with shelve.open(DB.PRODUCTION, "r") as farms_db:
-            harvested_farms = farms_db[county][version]
+            harvested_farms = farms_db[county][version].values()
         proof_data = load_excel_data(csv_file)
         cleaned_data = clean_excel_data(proof_data)
         final_documents = build_catalogue_documents(cleaned_data, harvested_farms, test_mode)
