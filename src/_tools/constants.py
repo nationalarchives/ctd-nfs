fb95fd15741d6afc1207eb381c5f04b9cd37bc83
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-18 11:24:26 +0100

    feat: update expression to allow for TEST names

--- src/_tools/constants.py
+++ src/_tools/constants.py
@@ -146,7 +146,7 @@ class RegexNamespace:
     SURVEY_YEARS = re.compile(r'^((19)?(41|42|43))$')
     IMAGE_ID = re.compile(r"""^66\/MAF\/32\/(?P<id>.*?)\.jpg$""")
     TRANSCRIPTION_ID = re.compile(r"""^[A-Z]{2}\d{6}$""")
-    VERSION = re.compile(r"""^.*?_HarvesterIN_(?P<version>V\d)-\d{8}-\d\d/$""")
+    PUBLISHING_VERSION = re.compile(r"""^.*?_HarvesterIN_(?P<version>V\d)-.*?$""")
 
 
 class PathNamespace:
