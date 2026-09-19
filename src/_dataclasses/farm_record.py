70c3ce65ed4831bafaee74cec64ec1a046669d73
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-19 11:01:52 +0100

    feat: change input data fro dataclass to PublishedFarm instance

--- src/_dataclasses/farm_record.py
+++ src/_dataclasses/farm_record.py
@@ -7,17 +7,18 @@ from urllib import parse
 
 import requests
 
+from src._dataclasses.farm_combine import PublishedFarm
 from src._tools.constants import DISCOVERY
 
 
 @dataclass
 class DiscoveryMAF32:
-    farm: dict
+    farm: PublishedFarm
     update_scope: str = DISCOVERY.UPDATE_SCOPE['new_record_with_digital_files']
 
     @property
     def parent_id (self) -> str:
-        ref = self.farm['catalogue_reference'].rsplit("/", maxsplit=1)[0]
+        ref = self.farm.catalogue_reference.rsplit("/", maxsplit=1)[0]
         ref_url_safe = parse.quote(ref)
 
         api_query = fr"{DISCOVERY.API_URI}/records/v1/collection/{ref_url_safe}"
@@ -45,16 +46,16 @@ class DiscoveryMAF32:
     @property
     def scope_and_content(self) -> dict:
         description_fields = {
-            'Farm Reference': f"{self.farm['farm_number']}.",
-            'Farm Name(s)': f"{self.farm['farm_name']}.",
-            'Addressee(s)': f"{self.farm['addressee']}.",
-            'Farmer(s) or Occupier(s)': f"{self.farm['farmer']}.",
-            'Landowner(s)': f"{self.farm['landowner']}.",
-            'Acreage(s)': f"{self.farm['acreage']}.",
-            'OS Sheet Number(s)': f"{self.farm['os_sheet_number']}.",
-            'Field Information Date(s)': f"{self.farm['field_info_date']}.",
-            'Primary Record Date(s)': f"{self.farm['primary_record_date']}.",
-            'Record consists of': f"{self.farm['forms']}.",
+            'Farm Reference': f"{self.farm.farm_reference}.",
+            'Farm Name(s)': f"{self.farm.farm.name}.",
+            'Addressee(s)': f"{self.farm.addressee}.",
+            'Farmer(s) or Occupier(s)': f"{self.farm.farmer}.",
+            'Landowner(s)': f"{self.farm.landowner}.",
+            'Acreage(s)': f"{self.farm.acreage}.",
+            'OS Sheet Number(s)': f"{self.farm.os_sheet_number}.",
+            'Field Information Date(s)': f"{self.farm.field_info_date}.",
+            'Primary Record Date(s)': f"{self.farm.primary_record_date}.",
+            'Record consists of': f"{self.farm.forms}.",
         }
 
         description = [
@@ -74,16 +75,16 @@ class DiscoveryMAF32:
                 'format': "jpg",
                 'name': f"66/MAF/32/{id}.jpg",
             }
-            for (id, name) in zip(re.split(r"[;,] *", self.farm['file_ids']), re.split(r"[;,] *", self.farm['file_names']))
+            for (id, name) in zip(re.split(r"[;,] *", self.farm.file_ids), re.split(r"[;,] *", self.farm.file_names))
         ]
 
     def to_dict(self) -> dict:
-        _, reference_part = self.farm['catalogue_reference'].rsplit("/", maxsplit=1)
+        _, reference_part = self.farm.catalogue_reference.rsplit("/", maxsplit=1)
         return { 
             'record': {
-                'iaid': self.farm['farm_id'],
-                'citableReference': self.farm['catalogue_reference'],
-                'replicaId': self.farm['replica_id'],
+                'iaid': self.farm.id,
+                'citableReference': self.farm.catalogue_reference,
+                'replicaId': self.farm.replica_id,
                 'parentId': self.parent_id,
                 'scopeContent': self.scope_and_content,
                 'referencePart': reference_part,
@@ -91,7 +92,7 @@ class DiscoveryMAF32:
             'updateScope': self.update_scope,
             'replica': {
                 'files': self.files,
-                'replicaId': self.farm['replica_id'],
+                'replicaId': self.farm.replica_id,
                 'origination': "DigitalSurrogate",
                 'totalSize': None,
             }
