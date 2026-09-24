2eee10919e7fb0a7b347e8638876b2e549721677
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-24 17:40:32 +0100

    fix: add missing part of import path

--- src/preview_builder.py
+++ src/preview_builder.py
@@ -4,7 +4,7 @@ import pprint
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from _dataclasses.farm_record import DiscoveryMAF32
+from src._dataclasses.farm_record import DiscoveryMAF32
 from src._tools.constants import PATH
 from src._tools.helpers import clean_excel_data, load_excel_data
 from src._tools.logging_setup import create_logger
