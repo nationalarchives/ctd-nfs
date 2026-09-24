578dd34014e32bdc8fa8e11c1d825685cc1d14ed
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-24 22:22:53 +0100

    fix; replace HarvestedFarm import with PublishedFarm

--- src/preview_builder.py
+++ src/preview_builder.py
@@ -4,7 +4,7 @@ import pprint
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from src._dataclasses.farm_combine import HarvestedFarm, PublishedFarm
+from src._dataclasses.farm_combine import PublishedFarm
 from src._dataclasses.farm_record import DiscoveryMAF32
 from src._tools.constants import PATH
 from src._tools.logging_setup import create_logger
