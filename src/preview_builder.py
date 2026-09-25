1be6176de558ff31d98b92bc717474d447c2b75f
Ralph Bruno <ralph.bruno@nationalarchives.gov.uk>
2026-09-25 07:19:07 +0100

    feat: update preview file name to use Path methods feat: replace suffix

--- src/preview_builder.py
+++ src/preview_builder.py
@@ -76,7 +76,7 @@ def create_html_preview(proof_file: Path, harvested_farms: dict) -> None:
     ]
     context = create_html_preview_context(published_farms, county)
 
-    previews_file_name = f"{proof_file_name.replace('_HarvesterOUT_', '_DiscoveryPreviews_')}.html"
+    previews_file_name = f"{proof_file.name.replace('_HarvesterOUT_', '_DiscoveryPreviews_').replace('xlsx', 'html')}"
     write_html_page(context, county, previews_file_name)
 
 
